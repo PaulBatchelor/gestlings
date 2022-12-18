e27d71bfde864061c3cb36663f7b3b8d819cd5d9
paul <paul@noemail.net>
2022-12-18 20:40:42 +0000

    more words and code added to morpheme
    FossilOrigin-Name: 4842bcfbb7c0f1200d960b9360f0e06669a97327bdc43dae849b6aac62f7b6bc

--- morpheme/test.lua
+++ morpheme/test.lua
@@ -4,61 +4,21 @@ path = require("path/path")
 pprint = require("util/pprint")
 lang = require("morpheme/testlang")
 
-Space = lpeg.S(" \n\t")^0
-Duration = lpeg.C(lpeg.R("09")^1)
-Value = lpeg.C(lpeg.R("az","AZ")^1)
-Behavior = lpeg.S("~+/_")
-
 lil([[
 gmemnew mem
 gestvmnew gvm
 ]])
 
-m = morpheme.morpheme({
-    a={{60, 3, 2}, {67, 1, 2}, {58, 2, 3}},
-    b={{63, 1, 2}, {65, 1, 2}, {63, 1, 2}, {62, 1, 2}}
-}, {1, 3})
 
-function verts(m)
-    local out = {}
-    for pname, p in pairs(m) do
-        out[pname] = {}
-        for k, v in pairs(p) do
-            out[pname][k] = path.vertex(v)
-        end
-    end
-    return out
-end
+append_morpheme = morpheme.appender(path)
 
-function append(m, mp)
-    for pname, p in pairs(m) do
-        if mp[pname] == nil then
-            mp[pname] = {}
-        end
-        for k, v in pairs(p) do
-            table.insert(mp[pname], path.vertex(v))
-        end
-    end
-end
-
-function append_morpheme(mp, r, m)
-    append(morpheme.morpheme(m, r), mp)
-end
-
-function compile_paths(words, mv)
-    for label, p in pairs(mv) do
-        tal.label(words, label)
-        path.path(tal, words, p)
-        tal.jump(words, label)
-    end
-end
 
 words = {}
 
-tal.start(words)
 
---mp = verts(m)
 mp = {}
+
+tal.start(words)
 
 append_morpheme(mp, {1, 3}, {
     a={{60, 3, 2}, {67, 1, 2}, {58, 2, 3}},
@@ -85,13 +45,12 @@ function seq(str)
     return lang.eval(str, notes)
 end
 
-
 append_morpheme(mp, {1, 3}, {
     a=seq("d1 s D2^ Me2~"),
     b=seq("d2~ r me f s f tt1 d r2")
 })
 
-compile_paths(words, mp)
+morpheme.compile(tal, path, words, mp)
 
 tal.compile_words(words, "mem", "[grab gvm]")
 
