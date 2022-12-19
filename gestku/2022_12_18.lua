512bdfa61c1de32d6194c9bd9a4a51017fa39ba0
paul <paul@noemail.net>
2022-12-19 02:28:15 +0000

    finished up gestku
    FossilOrigin-Name: e51387aac3a5e3891196b42ab0ac8da304931f0b42548d1a0469903f11c3058d

--- gestku/2022_12_18.lua
+++ gestku/2022_12_18.lua
@@ -1,4 +1,5 @@
 --[[
+INTERNAL MALFUNCTION
 -- <@>
 dofile("gestku/2022_12_18.lua")
 rtsetup()
@@ -99,9 +100,12 @@ end
 
 -- <@>
 notes = {
+    ddd = 36,
+    dd = 48,
     tt = 59,
     d = 60,
     r = 62,
+    ra = 61,
     me = 63,
     f = 65,
     s = 67,
@@ -171,6 +175,23 @@ end
 -- </@>
 
 -- <@>
+function pat(p)
+    return {
+        p=p.p or seq("d1"),
+        g=p.g or gate("o_ c"),
+        mod=p.mod or s16("b1_"),
+        car=p.car or s16("b1_"),
+        a=p.a or s16("b1_"),
+        d=p.d or s16("c1_"),
+        i=p.i or s16("a1_"),
+        r=p.r or s16("a1_"),
+        sz=p.sz or s16("o1_"),
+        fdbk=p.fdbk or s16("c_"),
+    }
+end
+-- </@>
+
+-- <@>
 A = {
     p=seq("d1 s D2^ Me2~"),
     g=gate("o_ c o c o2 c o c"),
@@ -179,15 +200,103 @@ A = {
     a=s16("b1_ o f"),
     d=s16("c1_ o2"),
     i=s16("a2/ o1^"),
-    r=s16("c2^ o1/"),
+    r=s16("o2^ c1/"),
     sz=s16("o1^"),
     fdbk=s16("c/ h"),
 }
 
+B = pat {
+    g=gate("o1_ c20"),
+    a=s16("a1_"),
+    d=s16("f1_"),
+    p=seq("D1_"),
+    r=s16("a1/ o3_"),
+    sz=s16("o_"),
+    i=s16("b1"),
+    car=s16("c1"),
+    mod=s16("f1"),
+}
+
+C = pat {
+    p=seq("d1/ ddd3~"),
+    g=gate("o3_"),
+    a=s16("a1_"),
+    d=s16("f1_"),
+    i=s16("a1/ o a/ o/ c~"),
+    fdbk=s16("m1_"),
+    car=s16("b1_"),
+    mod=s16("f1/ b_"),
+    sz=s16("a1 n2^ f2_"),
+    r=s16("d1/ g2^ d"),
+}
+
+D = pat {
+    g=gate("o1_ c3 o1 c3 o3 c1 c4"),
+    a=s16("a1_"),
+    d=s16("c1_"),
+    p=seq("d1/ me1/"),
+    r=s16("g1_"),
+    sz=s16("l_"),
+    i=s16("b1/"),
+    car=s16("b1"),
+    mod=s16("b1"),
+    fdbk=s16("b1"),
+}
+
+E = pat {
+    g=gate("o1_ c3 o1 c3 o3 c1 c4 o c"),
+    a=s16("a1_"),
+    d=s16("c1_"),
+    p=seq("me3/ d1_"),
+    r=s16("g1_"),
+    sz=s16("l_"),
+    i=s16("b1/"),
+    car=s16("b1"),
+    mod=s16("b1"),
+    fdbk=s16("b1"),
+}
+
+F = pat {
+    p = seq("d2/ ra1_"),
+    car=s16("d1_"),
+    mod=s16("c1_"),
+    g=gate("o1_"),
+}
+
+I = pat {
+    p=seq("me1~ f s2 d2^ l"),
+    g=gate("o1_ c o c o2 c o6 c2"),
+    i=s16("b1/ f3~"),
+    r=s16("d1/ g1_"),
+    sz=s16("m3~ o1_"),
+}
+
+
+H = pat {
+    p=seq("ddd3_"),
+    g=gate("o3_"),
+    a=s16("a1_"),
+    d=s16("f1_"),
+    i=s16("o1_"),
+    fdbk=s16("m1_"),
+    car=s16("b1_"),
+    mod=s16("f1/ b1_"),
+    sz=s16("a1 n2^ f2_"),
+    r=s16("d1/ g2^ d"),
+}
+
 SEQ = {
-    {A, {1, 3}},
     {A, {1, 2}},
-    {A, {1, 1}},
+    {D, {2, 1}},
+    {F, {1, 1}},
+    {C, {4, 1}},
+    {H, {3, 1}},
+    {C, {4, 1}},
+    {C, {2, 1}},
+    {I, {1, 1}},
+    {E, {1, 1}},
+    {C, {1, 5}},
+    {B, {1, 4}},
 }
 -- </@>
 
@@ -244,15 +353,11 @@ function sound()
     lil("mul zz zz")
     gest16("r", 0, 1)
     lil("crossfade zz zz zz")
+    lil("peakeq zz 80 80 2")
+    lil("tgate [tick] 10.5; smoother zz 0.01; mul zz zz")
     lil("regget 0; unhold zz; gldone [grab glive]")
     lil("unholdall")
 end
--- </@>
-
--- <@>
--- </@>
-
--- <@>
 
 function run()
     sound()
@@ -266,16 +371,17 @@ end
 
 function G.symbol()
     return [[
----###---
--###-###-
--#-----#-
--#-#-#-#-
-##-----##
-#-------#
-#-#-#-#-#
-#-#-#-#-#
-#-------#
-#########
+####--##-#
+----##--#-
+####--##--
+----##--#
+####--#---
+----##---#
+-------#--
+----------
+------#-#-
+--#-------
+-----#----
 ]]
 end
 
