81e9ba5213847e01f9d447360826e368566792a3
paul <paul@noemail.net>
2022-12-19 12:06:58 +0000

    some initial gestku work
    FossilOrigin-Name: 29a7a58820a2bdf363940198505c1f64320dd2300e9eea4d4f577e6e65a6f102

---
+++ gestku/2022_12_19.lua
@@ -0,0 +1,319 @@
+--[[
+-- <@>
+dofile("gestku/2022_12_19.lua")
+rtsetup()
+setup()
+-- </@>
+
+-- <@>
+lil("glreset [grab glive]")
+-- </@>
+--]]
+
+-- <@>
+G = {}
+
+function G.symbol()
+    return [[
+####--##-#
+----##--#-
+####--##--
+----##--#
+####--#---
+----##---#
+-------#--
+----------
+------#-#-
+--#-------
+-----#----
+]]
+end
+
+tal = require("tal/tal")
+path = require("path/path")
+morpheme = require("morpheme/morpheme")
+pprint = require("util/pprint")
+morpho = require("morpheme/morpho")
+
+append = morpheme.appender(path)
+
+function rtsetup()
+lil([[
+hsnew hs
+rtnew [grab hs] rt
+
+func out {} {
+    hsout [grab hs]
+    hsswp [grab hs]
+}
+
+func playtog {} {
+    hstog [grab hs]
+}
+]])
+end
+
+function setup()
+lil([[
+gmemnew mem
+glnew glive
+]])
+end
+
+-- </@>
+
+-- <@>
+function patch_setup(tempo)
+    out = string.format([[
+glswapper [grab glive]
+param [expr %d / 60]
+phasor zz 0
+hold zz
+regset zz 0
+]], tempo)
+    return out
+end
+-- </@>
+
+-- <@>
+function gestvmnode(glive, membuf, program, conductor)
+    lil(string.format(
+        "gestvmnode %s [gmemsym [grab %s] %s] %s",
+        glive, membuf, program, conductor))
+
+end
+-- </@>
+
+-- <@>
+function mkvoice(pitch, gate, mi, fb)
+    lil("regget 1")
+    gestvmnode("[glget [grab glive]]",
+        "mem",
+        pitch,
+        "[regget 0]")
+
+    if (mi == nil) then mi = 1.3 end
+    if (fb == nil) then fb = 0 end
+    patch = {
+        "mtof zz",
+        string.format("fmpair zz zz 1 1 %g %g", mi, fb),
+        -- "blsaw zz",
+        "mul zz 0.1",
+        -- "butlp zz 500"
+    }
+
+    for _, str in pairs(patch) do
+        lil(str)
+    end
+
+    if (gate ~= nil) then
+        gestvmnode("[glget [grab glive]]",
+            "mem",
+            gate,
+            "[regget 0]")
+        lil("smoother zz 0.02")
+        lil("mul zz zz")
+    end
+end
+-- </@>
+
+-- <@>
+notes = {
+    ff = -7,
+    ss = -5,
+    ll = -3,
+    tt = -1,
+    d = 0,
+    r = 2,
+    m = 4,
+    f = 5,
+    s = 7,
+    l = 9,
+    t = 11,
+    D = 12,
+    R = 14,
+    M = 16,
+    F = 17,
+    S = 19,
+    L = 21,
+    T = 23,
+    DD = 24,
+}
+
+for k,v in pairs(notes) do
+    notes[k] = v + 58
+end
+
+function seq(str)
+    return morpho.eval(str, notes)
+end
+-- </@>
+
+-- <@>
+gates = {
+    o = 1,
+    c = 0
+}
+
+function gate(str)
+    return morpho.eval(str, gates)
+end
+-- </@>
+
+-- <@>
+step16 = {
+    a = 0,
+    b = 1,
+    c = 2,
+    d = 3,
+    e = 4,
+    f = 5,
+    g = 6,
+    h = 7,
+    h = 8,
+    i = 9,
+    j = 10,
+    k = 11,
+    l = 12,
+    m = 13,
+    n = 14,
+    o = 15,
+}
+
+function s16(str)
+    return morpho.eval(str, step16)
+end
+-- </@>
+
+-- <@>
+function gesture(name)
+    gestvmnode("[glget [grab glive]]",
+               "mem",
+               name,
+               "[regget 0]")
+end
+
+function gest16(name, mn, mx)
+    gesture(name)
+    lil(string.format(
+        "div zz 16; scale zz %g %g", mn, mx))
+end
+-- </@>
+
+-- <@>
+function pat(p)
+    return {
+        ga=p.ga or gate("o1_"),
+        pa=p.pa or seq("d1~"),
+
+        gb=p.gb or gate("o1_"),
+        pb=p.pb or seq("r1~"),
+
+        gc=p.gc or gate("o1_"),
+        pc=p.pc or seq("m1~"),
+
+        gd=p.gd or gate("o1_"),
+        pd=p.pd or seq("s1~"),
+
+        ge=p.ge or gate("o1_"),
+        pe=p.pe or seq("t1~"),
+
+    }
+end
+
+A = pat { }
+
+B = pat {
+    pe = seq("l~"),
+    pc = seq("f~"),
+}
+
+SEQ = {
+    {A, {1, 3}},
+    {B, {1, 3}},
+}
+-- </@>
+
+-- <@>
+function articulate()
+    words = {}
+
+    mp = {}
+
+    tal.start(words)
+
+    for _,s in pairs(SEQ) do
+        append(mp, s[2], s[1])
+    end
+
+
+    morpheme.compile(tal, path, words, mp)
+
+    tal.compile_words(words, "mem", "[glget [grab glive]]")
+end
+-- </@>
+
+-- <@>
+function voice(id)
+    lil("regget 1")
+    gesture("p" .. id)
+    lil("mtof zz")
+    param(1)
+    param(1)
+    param(1)
+    param(0)
+    lil("fmpair zz zz zz zz zz zz")
+
+    gesture("g" .. id)
+    param(0.01)
+    param(0.01)
+    lil("envar zz zz zz")
+    lil("mul zz zz")
+end
+-- </@>
+
+-- <@>
+function param(x)
+    lil(string.format("param %g", x))
+end
+function sound()
+    lil(patch_setup(70))
+    articulate()
+    lil("regset [gensine [tabnew 8192]] 1")
+
+    voice("a")
+    voice("b")
+    lil("add zz zz")
+    voice("c")
+    lil("add zz zz")
+    voice("d")
+    lil("add zz zz")
+    voice("e")
+    lil("add zz zz")
+
+    lil("mul zz [dblin -17]")
+    lil("dup; dup")
+
+    param(0.9)
+    lil("param 10000")
+    lil("bigverb zz zz zz zz; drop; dcblocker zz")
+    lil("mul zz [dblin -15]")
+    lil("add zz zz")
+
+    --lil("tgate [tick] 10.5; smoother zz 0.01; mul zz zz")
+    lil("regget 0; unhold zz; gldone [grab glive]")
+    lil("unholdall")
+end
+
+function run()
+    sound()
+    lil("out")
+end
+
+function G.patch()
+    setup()
+    sound()
+end
+
+return G
+-- </@>
