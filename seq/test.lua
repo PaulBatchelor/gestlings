56ce1aac6d97f7459680ee98deb001f04a0bbed3
paul <paul@noemail.net>
2022-12-30 19:02:26 +0000

    added gate functions to seq
    FossilOrigin-Name: ea60a1795254699542aa3545c98cfc443da894dbc4188a924d15d931973b5f33

--- seq/test.lua
+++ seq/test.lua
@@ -7,9 +7,11 @@ seq = require("seq/seq")
 gest = require("gest/gest")
 
 s16 = seq.seqfun(morpho)
+gate = seq.gatefun(morpho)
 
 A = {
-    seq = s16("a/ d_ f o2~"),
+    seq = s16("a/ d_ f^ o2~"),
+    gate = gate("o2_ o1 c o2 o4"),
 }
 
 mseq = {
@@ -36,6 +38,9 @@ lil(string.format("mul zz %g", 1.0 / 16))
 lil("scale zz 48 70")
 lil("mtof zz")
 lil("blsaw zz; butlp zz 500; mul zz 0.3")
+g:node("gate")
+lil("envar zz 0.008 0.03")
+lil("mul zz zz")
 lil("gldone [grab glive]")
 lil("wavout zz test.wav")
 lil("computes 10")
