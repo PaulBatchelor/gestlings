91b32ba5e47fd266a91bbef285744a5d39afc936
paul <paul@noemail.net>
2022-12-31 22:01:07 +0000

    working sends and throws with sig
    FossilOrigin-Name: 8e4068053d891176055759775348aabb9e42f2b785f842030d3402a8a315a377

--- sig/test.lua
+++ sig/test.lua
@@ -11,6 +11,8 @@ lil("metro 6")
 b:hold()
 b:unhold()
 
+b:zero()
+
 lil("metro [rline 2 4 2]")
 a:hold()
 
@@ -18,11 +20,26 @@ s:get()
 lil("env zz 0.001 0.01 0.1")
 lil("sine 600 0.3")
 lil("mul zz zz")
+b:throw(-6)
 
 a:get()
 lil("env zz 0.001 0.01 0.1")
 lil("sine 700 0.3")
 lil("mul zz zz")
+b:throw(-6)
 lil("add zz zz")
 
+b:get()
+lil([[
+dup; bigverb zz zz 0.9 10000;
+drop;
+mul zz [dblin -3];
+dcblocker zz
+add zz zz
+]]
+)
+
+a:unhold()
+b:unhold()
+s:unhold()
 lil("wavout zz test.wav; computes 10")
