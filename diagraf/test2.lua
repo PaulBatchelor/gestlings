78dc51ca70c4024c9504881e1ef84c0559071109
paul <paul@noemail.net>
2023-01-08 15:41:48 +0000

    diagraf: test2 generates working dot diagram
    FossilOrigin-Name: e7dd1ec99b404ec1fc02eb5226bc03f5c15eaf056cf10b7d9ba2342ff70b192c

---
+++ diagraf/test2.lua
@@ -0,0 +1,47 @@
+diagraf = require("diagraf/diagraf")
+pprint = require("util/pprint")
+nodes = require("diagraf/nodes")
+sig = require("sig/sig")
+
+g = diagraf.Graph:new{debug=true, sig=sig}
+
+n = {}
+nodes.nodes(diagraf.Node, g, n)
+
+s1 = n.blsaw()
+lfo = n.sine{freq=1.23, amp=1}
+lfo:label("LFO generator")
+gain = n.mul{b=0.5}
+lpf = n.butlp{cutoff=300}
+
+con = g:connector()
+
+bias = n.biscale{min=200, max=500}
+con(lfo, bias.input)
+con(bias, s1.freq)
+con(s1, gain.a)
+
+lpf_lfo = n.biscale{min=321, max=1234}
+con(lfo, lpf_lfo.input)
+con(lpf_lfo, lpf.cutoff)
+
+con(gain, lpf.input)
+
+out = lpf
+con(out, n.wavout().input)
+
+-- add an envelope
+met = n.metro{rate = 2}
+env = n.env{}
+con(met, env.trig)
+env_scaled = n.mul{b=0.5}
+con(env, env_scaled.a)
+con(env_scaled, gain.b)
+
+g:process()
+l = topsort(g.edges)
+g:nsort_rec(l, g.nodes[l[#l]], #l)
+g:setters_to_first_getters(l)
+g:nsort_rec(l, g.nodes[l[#l]], #l)
+g:postprocess(l)
+g:dot("diagraf_test.dot")
