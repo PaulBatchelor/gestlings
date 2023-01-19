bcc4e0881e36e2935bc187061aaa214c9f24a401
paul <paul@noemail.net>
2023-01-19 11:29:21 +0000

    gestku completed. abstracted away some components into gestku boilerplate
    FossilOrigin-Name: 38a09fe47d4dd8292b44bfb7b8b42464b5c38ecc51ff75ef20dd1e9a78dcb756

--- gestku/2023_01_18.lua
+++ gestku/2023_01_18.lua
@@ -1,56 +1,28 @@
 --[[
+Whistle Antiphon
 -- <@>
 dofile("gestku/2023_01_18.lua")
 G:rtsetup()
 G:setup()
 -- </@>
-
--- <@>
-lil("glreset [grab glive]")
-lil("unholdall")
--- </@>
 --]]
 
 -- <@>
 gestku = require("gestku/gestku")
-tal = require("tal/tal")
-path = require("path/path")
-morpheme = require("morpheme/morpheme")
-pprint = require("util/pprint")
-morpho = require("morpheme/morpho")
 whistle = require("whistle/whistle")
-core = require("util/core")
-sig = require("sig/sig")
-diagraf = require("diagraf/diagraf")
-sr = require("sigrunes/sigrunes")
-mseq = require("morpheme/mseq")
-seq = require("seq/seq")
-nrt = require("nrt/nrt")
 
-s16 = seq.seqfun(morpho)
-
-G = gestku:new {
-    tal = tal,
-    morpheme = morpheme,
-    path = path
-}
+s16 = gestku.seq.seqfun(gestku.morpho)
+gest16 = gestku.gest.gest16fun(gestku.sr, gestku.core)
+G = gestku:new()
 
 function G.symbol()
     return [[
-###-------
-#-#-------
-###-------
-----------
--#--------
-----------
--#--------
-----------
--#--------
-----------
-----------
--------###
--------#-#
--------###
+#-#-####
+--#----#
+#-##-#--
+#-#--###
+#-##-#--
+-----#--
 ]]
 end
 -- </@>
@@ -59,10 +31,9 @@ end
 -- <@>
 
 function solf(s)
-    return nrt.eval(s, {base=60})
+    return gestku.nrt.eval(s, {base=61})
 end
 
-mel2 = "drm rmf mfs ftD"
 mel3 = "d4 s2 t-8 D^ l4~ f d2~ r-4/ d2.~"
 vocab = {
     A = {
@@ -78,34 +49,17 @@ vocab = {
 }
 
 SEQ = "11[A]A2[S]"
-SEQ = mseq.parse(SEQ, vocab)
+SEQ = gestku.mseq.parse(SEQ, vocab)
 
 -- </@>
 
 -- <@>
-function gest16(gst, name, cnd, mn, mx)
-    local pn = sr.paramnode
-    local lvl = core.liln
-
-	local node = pn(sr.scale) {
-		input = pn(sr.mul) {
-			a = pn(gst:node()) {
-				name = name,
-				conductor = lvl(cnd:getstr())
-			},
-			b = 1.0 / 16.0
-		},
-		min = mn,
-		max = mx
-	}
-
-	return node
-end
-
 function G:sound()
-    local lvl = core.liln
+    local lvl = gestku.core.liln
+    local sr = gestku.sr
     local pn = sr.paramnode
     local ln = sr.lilnode
+    local sig = gestku.sig
     local cnd = sig:new()
     local gst = G.gest
 
@@ -140,18 +94,14 @@ function G:sound()
 		timbre = tg,
 		amp = ag,
         sig = sig,
-        core = core,
-        diagraf = diagraf,
+        core = gestku.core,
+        diagraf = gestku.diagraf,
         sigrunes = sr
     }
 
 	l = g:generate_nodelist()
 	g:compute(l)
 
-    -- lil("sine 1000 0.3")
-    -- cnd:get()
-    -- lil("phsclk zz 1")
-    -- lil("env zz 0.001 0.001 0.001; mul zz zz; add zz zz")
 	gst:done()
     cnd:unhold()
 
