ffe840143875a5d9df4e4b1c8c5efaf49572508a
paul <paul@noemail.net>
2023-01-21 01:47:24 +0000

    more warble elaborations
    FossilOrigin-Name: 0ff933aefe2cf9f1f06b83e806edffdd675035df340f06b46ee2705b49688604

--- warble/test.lua
+++ warble/test.lua
@@ -1,6 +1,8 @@
 core = require("util/core")
 sr = require("sigrunes/sigrunes")
 diagraf = require("diagraf/diagraf")
+pprint = require("util/pprint")
+sig = require("sig/sig")
 
 nd = sr.node
 ln = core.liln
@@ -10,40 +12,87 @@ plf = core.plilf
 lil("gensine [tabnew 8192]")
 lil("regset zz 0; regmrk 0")
 
-g = diagraf.Graph:new()
+g = diagraf.Graph:new{sig=sig}
 
 ng  = core.nodegen(diagraf.Node, g)
 pg  = core.paramgen(ng)
 con = g:connector()
+prmf = core.paramf
+
+p = {
+    freq = lf("rline 200 400 1")
+}
+
+freq = pg(p.freq or prmf(330))
 
 -- sr.lilnode_debug(true)
 fm = ng(sr.fmpair) {
     tab = plf("regget 0"),
-    freq = 330,
     mi = 1.5,
     car = 1.0,
-    mod = 3.0,
+    mod = 1.0,
     fdbk = 0.4,
 }
 
-tab = ln("regget 0")
+scaler = ng(sr.scale)
 
--- lil("mul zz 0.2")
--- lil([[
--- regget 0
--- param 440
--- param 1
--- param 1
--- param 1
--- param 0
--- fmpair zz zz zz zz zz zz
--- mul zz 0.2
--- ]])
+mul = ng(sr.mul)
+nz = ng(sr.noise)()
+lpf = ng(sr.butlp)()
+hpf = ng(sr.buthp){cutoff=300}
+con(nz, hpf.input)
+con(hpf, lpf.input)
+freqmul = mul{b = 2.0}
+con(freq, freqmul.a)
+con(freqmul, lpf.cutoff)
 
+envar = ng(sr.envar)
+
+cf = ng(sr.crossfade) {
+    pos = 0.5
+}
+
+cfenv = envar {
+    gate = plf("metro 10; tgate zz 0.01"),
+    atk = 0.03,
+    rel = 0.01,
+}
+sclcf = scaler {
+    min = 0.0,
+    max = 1.0
+}
+con(cfenv, sclcf.input)
+con(sclcf, cf.pos)
+
+
+con(fm, cf.a)
+con(lpf, cf.b)
+
+osc = cf
+
+con(freq, fm.freq)
+
+sclamp = scaler {
+    min = 0.0,
+    max = 1.0
+}
+
+
+ampenv = envar {
+    gate = plf("metro 1.5; tgate zz 0.01"),
+    atk = 0.003,
+    rel = 0.9,
+}
+
+con(ampenv, sclamp.input)
+
+ascl_mul = mul()
+con(osc, ascl_mul.a)
+con(sclamp, ascl_mul.b)
+g:dot("warble.dot")
 l = g:generate_nodelist()
-pprint = require("util/pprint")
 
-pprint(tab)
 g:compute(l)
+
 lil("mul zz 0.2; wavout zz test.wav")
 lil("computes 10")
