8a25db0ed7155bc6f0e4fd6aa5a9bb306a1a12c8
paul <paul@noemail.net>
2023-02-07 17:36:12 +0000

    gestku: refactors previous gestku with ftlist
    FossilOrigin-Name: 0b2816dc5f8dce2b0778237dc5bf0d09687dd495aae6b72743f6dc64b1f4cb4d

---
+++ gestku/2023_02_07.lua
@@ -0,0 +1,92 @@
+--[[
+down in the muck
+-- <@>
+dofile("gestku/2023_02_07.lua")
+G:rtsetup()
+G:setup()
+-- </@>
+--]]
+
+-- <@>
+gestku = require("gestku/gestku")
+G = gestku:new()
+
+function G.symbol()
+    return [[
+-----------
+-###-#-###-
+-----#-----
+-###-#-###-
+-----#-----
+-###-#-###-
+-----#-----
+-###-#-###-
+-----------
+]]
+end
+-- </@>
+
+-- <@>
+function G:init()
+    print("hello")
+    lil([[ftlnew ftl
+grab ftl
+gensinesum [tabnew 8192] "1 1 1 1 0 0 0.1 0.1"
+ftladd zz
+gensinesum [tabnew 8192] "1 1 0 0.5 0 0.2"
+ftladd zz
+drop
+]])
+end
+-- </@>
+
+-- <@>
+function G:sound()
+lil([[
+sine 0.2 1
+hold zz
+regset zz 0
+
+sine [rline 4 12 1] 1
+hold zz
+regset zz 1
+
+ftlget [grab ftl] 0
+ftlget [grab ftl] 1
+oscmorph zz zz \
+[mtof [smoother [add 27 [mul [tog [metro [expr 1 / 4 ] ] ] 3] ] [param 0.1] ] ] \
+[biscale [regget 1] 0 1] \
+[biscale [regget 0] 0.1 0.9]
+mul zz 0.1
+
+ftlget [grab ftl] 0
+ftlget [grab ftl] 1
+oscmorph zz zz \
+[mtof 46] \
+[biscale [regget 1] 0 1] \
+[biscale [regget 0] 0.1 0.2]
+mul zz [rline 0.1 0.2 0.1]
+
+add zz zz
+softclip zz 3.5
+valp1 zz 1000
+
+dup
+
+vardelay zz 0.9 0.2 2.0
+buthp zz 2000
+mul zz [dblin 8]
+add zz zz
+
+unhold [regget 0]
+unhold [regget 1]
+]])
+end
+-- </@>
+
+function run()
+    G:run()
+end
+
+return G
+-- </@>
