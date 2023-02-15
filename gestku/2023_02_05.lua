e63e93a2ea0c001c07fcbc90152e34b425f02281
paul <paul@noemail.net>
2023-02-15 11:02:54 +0000

    gesktu: 'in the muck' updates
    FossilOrigin-Name: 470811ae29abad05aa2105bab46ad282b7a0338c14d5067ab6e34c76f7cff07d

--- gestku/2023_02_05.lua
+++ gestku/2023_02_05.lua
@@ -37,28 +37,28 @@ sine [rline 4 12 1] 1
 hold zz
 regset zz 1
 
-gensinesum [tabnew 8192] "1 1 1 1 0 0 0.1 0.1"
-gensinesum [tabnew 8192] "1 1 0 0.5 0 0.2"
+gensinesum [tabnew 8192] "1 1 1 1 0 0 0.1 0.1" 1
+gensinesum [tabnew 8192] "1 1 0 0.5 0 0.2" 1
 oscmorph zz zz \
 [mtof [smoother [add 27 [mul [tog [metro [expr 1 / 4 ] ] ] 3] ] [param 0.1] ] ] \
 [biscale [regget 1] 0 1] \
-[biscale [regget 0] 0.1 0.9]
-mul zz 0.1
+[biscale [regget 0] 0.1 1.9]
+mul zz 2.5
 
-gensinesum [tabnew 8192] "1 1 1 1 0 0 0.1 0.1"
-gensinesum [tabnew 8192] "1 1 0 0.5 0 0.2"
+gensinesum [tabnew 8192] "1 1 1 1 0 0 0.1 0.1" 1
+gensinesum [tabnew 8192] "1 1 0 0.5 0 0.2" 1
 oscmorph zz zz \
 [mtof 46] \
 [biscale [regget 1] 0 1] \
-[biscale [regget 0] 0.1 0.2]
+[biscale [regget 0] 0.1 1.0]
 mul zz [rline 0.1 0.2 0.1]
+mul zz 4.5
 
 add zz zz
 softclip zz 3.5
 valp1 zz 1000
 
 dup
-
 vardelay zz 0.9 0.2 2.0
 buthp zz 2000
 mul zz [dblin 8]
