7118addc23a5df8fedc226f4c8e74bc433696983
paul <paul@noemail.net>
2023-03-07 11:47:22 +0000

    more code shuffling
    FossilOrigin-Name: cf5adfffa39ffeec34196584b55c62b0d8cc3082002f755f0d71fcf4e5357f81

--- gestku/bits/morpher.lua
+++ gestku/bits/morpher.lua
@@ -1,3 +1,44 @@
+function wtpos_values(name)
+    local wtpos = {"wtpos0", "wtpos1", "wtpos2", "wtpos3"}
+
+    for k, v in pairs(wtpos) do
+        wtpos[k] = v .. name
+    end
+
+    return wtpos
+end
+
+function seqnode(cnd, name)
+    local ln = gestku.core.liln
+    gestku.sr.node(G.gest:node()) {
+        name = "seq" .. name,
+        conductor = ln(cnd:getstr())
+    }
+end
+
+function gfmparamnode(cnd, op, param, name)
+    local ln = gestku.core.liln
+    gestku.sr.node(G.gest:node()) {
+        name = param .. op .. name,
+        conductor = ln(cnd:getstr())
+    }
+end
+
+function gmorphfmparam(gst, op, param, sig)
+    local cmd = string.format("gmorphfmparam %s %s %s %s",
+        gst, op, param, sig)
+    lil(cmd)
+end
+
+function gmorphfmnew(gst, ftl, wtpos, algo)
+    lil("gmorphfmnew " ..  gst:get() ..
+        " " .. ftl .. " " ..
+        "[" .. gst:gmemsymstr(wtpos[1]) .. "] " ..
+        "[" .. gst:gmemsymstr(wtpos[2]) .. "] " ..
+        "[" .. gst:gmemsymstr(wtpos[3]) .. "] " ..
+        "[" .. gst:gmemsymstr(wtpos[4]) .. "] " ..
+        algo)
+end
 function gfmgestparam(cnd, op, param, name)
     local sig = gestku.sig
 
