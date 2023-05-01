e70ba90c15efc5148d12881d74429f001172be9f
paul <paul@noemail.net>
2023-05-01 14:46:23 +0000

    encapsulated grammar generator into function
    FossilOrigin-Name: bf23fced1e31ea11be9a1d148998b1ca0fb7dba23ac76656311ff3eb669ada64

--- path/notate.lua
+++ path/notate.lua
@@ -11,24 +11,24 @@ for id, sym in pairs(symbols) do
     symtab[sym.name] = id
 end
 
-zero="zero"
-one="one"
-two="two"
-three="three"
-four="four"
-five="five"
-six="six"
-seven="seven"
-eight="eight"
-nine="nine"
-ten="ten"
-eleven="eleven"
-twelve="twelve"
-thirteen="thirteen"
-fourteen="fourteen"
-fifteen="fifteen"
-brackl="bracket_left"
-brackr="bracket_right"
+zero = "zero"
+one = "one"
+two = "two"
+three = "three"
+four = "four"
+five = "five"
+six = "six"
+seven = "seven"
+eight = "eight"
+nine = "nine"
+ten = "ten"
+eleven = "eleven"
+twelve = "twelve"
+thirteen = "thirteen"
+fourteen = "fourteen"
+fifteen = "fifteen"
+brackl = "bracket_left"
+brackr = "bracket_right"
 div = "divider"
 ratemulstart = "ratemulstart"
 ratemulend = "ratemulend"
@@ -90,71 +90,77 @@ end
 
 print(hexstr)
 
+function generate_path_grammar(symtab)
+    local Space = lpeg.S(" \n\t")^0
+    local LBrack = lpeg.P(string.format("%02x", symtab["bracket_left"]))
+    local RBrack = lpeg.P(string.format("%02x", symtab["bracket_right"]))
+    local One = lpeg.P(string.format("%02x", symtab["one"]))
+    local Two = lpeg.P(string.format("%02x", symtab["two"]))
+    local Three = lpeg.P(string.format("%02x", symtab["three"]))
+    local Four = lpeg.P(string.format("%02x", symtab["four"]))
+    local Five = lpeg.P(string.format("%02x", symtab["five"]))
+    local Six = lpeg.P(string.format("%02x", symtab["six"]))
+    local Seven = lpeg.P(string.format("%02x", symtab["seven"]))
+    local Eight = lpeg.P(string.format("%02x", symtab["eight"]))
+    local Nine = lpeg.P(string.format("%02x", symtab["nine"]))
+    local Ten = lpeg.P(string.format("%02x", symtab["ten"]))
+    local Eleven = lpeg.P(string.format("%02x", symtab["eleven"]))
+    local Twelve = lpeg.P(string.format("%02x", symtab["twelve"]))
+    local Thirteen = lpeg.P(string.format("%02x", symtab["thirteen"]))
+    local Fourteen = lpeg.P(string.format("%02x", symtab["fourteen"]))
+    local Fifteen = lpeg.P(string.format("%02x", symtab["fifteen"]))
+    local Zero = lpeg.P(string.format("%02x", symtab["zero"]))
+    local RateMulStart = lpeg.P(string.format("%02x", symtab["ratemulstart"]))
+    local RateMulEnd = lpeg.P(string.format("%02x", symtab["ratemulend"]))
+    local Divider = lpeg.P(string.format("%02x", symtab["divider"]))
+    local Linear = lpeg.P(string.format("%02x", symtab["linear"]))
+    local Step = lpeg.P(string.format("%02x", symtab["step"]))
+    local GlissBig = lpeg.P(string.format("%02x", symtab["gliss_big"]))
+    local GlissMedium = lpeg.P(string.format("%02x", symtab["gliss_medium"]))
+    local GlissSmall = lpeg.P(string.format("%02x", symtab["gliss_small"]))
+
+    local Symbol = LBrack + RBrack + One + Two + Three + Four
+    local Number =
+        Zero / "0" +
+        One / "1" +
+        Two / "2" +
+        Three / "3" +
+        Four / "4" +
+        Five / "5" +
+        Six / "6" +
+        Seven / "7" +
+        Eight / "8" +
+        Nine / "9" +
+        Ten / "a" +
+        Eleven / "b" +
+        Twelve / "c" +
+        Thirteen / "d" +
+        Fourteen / "e" +
+        Fifteen / "f"
+    local Behavior = Space * (
+        Linear / "linear" +
+        Step / "step" +
+        GlissBig / "gliss_large" +
+        GlissMedium / "gliss_medium" +
+        GlissSmall / "gliss_small"
+        ) * Space
+    local Nibble = Space * Number * Space
+    local Div = (Space * Divider * Space)^0
+    local Hex = Div * lpeg.Ct(Nibble*Nibble) * Div
+    local RateMul = RateMulStart * Hex * Div * Hex * RateMulEnd
+    local Value = lpeg.Ct(lpeg.Cg(Hex, "value") *
+                    lpeg.Cg(lpeg.Ct(RateMul), "ratemul")^0 *
+                    lpeg.Cg(Behavior, "behavior")^0
+                    )
+
+    local Path = LBrack * (Value)^0 * RBrack * Space
+    return Path
+end
+
 Space = lpeg.S(" \n\t")^0
-LBrack = lpeg.P(string.format("%02x", symtab["bracket_left"]))
-RBrack = lpeg.P(string.format("%02x", symtab["bracket_right"]))
-One = lpeg.P(string.format("%02x", symtab["one"]))
-Two = lpeg.P(string.format("%02x", symtab["two"]))
-Three = lpeg.P(string.format("%02x", symtab["three"]))
-Four = lpeg.P(string.format("%02x", symtab["four"]))
-Five = lpeg.P(string.format("%02x", symtab["five"]))
-Six = lpeg.P(string.format("%02x", symtab["six"]))
-Seven = lpeg.P(string.format("%02x", symtab["seven"]))
-Eight = lpeg.P(string.format("%02x", symtab["eight"]))
-Nine = lpeg.P(string.format("%02x", symtab["nine"]))
-Ten = lpeg.P(string.format("%02x", symtab["ten"]))
-Eleven = lpeg.P(string.format("%02x", symtab["eleven"]))
-Twelve = lpeg.P(string.format("%02x", symtab["twelve"]))
-Thirteen = lpeg.P(string.format("%02x", symtab["thirteen"]))
-Fourteen = lpeg.P(string.format("%02x", symtab["fourteen"]))
-Fifteen = lpeg.P(string.format("%02x", symtab["fifteen"]))
-Zero = lpeg.P(string.format("%02x", symtab["zero"]))
-RateMulStart = lpeg.P(string.format("%02x", symtab["ratemulstart"]))
-RateMulEnd = lpeg.P(string.format("%02x", symtab["ratemulend"]))
 Null = lpeg.P("00")
-Divider = lpeg.P(string.format("%02x", symtab["divider"]))
-Linear = lpeg.P(string.format("%02x", symtab["linear"]))
-Step = lpeg.P(string.format("%02x", symtab["step"]))
-GlissBig = lpeg.P(string.format("%02x", symtab["gliss_big"]))
-GlissMedium = lpeg.P(string.format("%02x", symtab["gliss_medium"]))
-GlissSmall = lpeg.P(string.format("%02x", symtab["gliss_small"]))
 
-Symbol = LBrack + RBrack + One + Two + Three + Four
-Number =
-    Zero / "0" +
-    One / "1" +
-    Two / "2" +
-    Three / "3" +
-    Four / "4" +
-    Five / "5" +
-    Six / "6" +
-    Seven / "7" +
-    Eight / "8" +
-    Nine / "9" +
-    Ten / "a" +
-    Eleven / "b" +
-    Twelve / "c" +
-    Thirteen / "d" +
-    Fourteen / "e" +
-    Fifteen / "f"
-Behavior = Space * (
-    Linear / "linear" +
-    Step / "step" +
-    GlissBig / "gliss_large" +
-    GlissMedium / "gliss_medium" +
-    GlissSmall / "gliss_small"
-    ) * Space
-Nibble = Space * Number * Space
-Div = (Space * Divider * Space)^0
-Hex = Div * lpeg.Ct(Nibble*Nibble) * Div
-RateMul = RateMulStart * Hex * Div * Hex * RateMulEnd
-Value = lpeg.Ct(lpeg.Cg(Hex, "value") *
-                lpeg.Cg(lpeg.Ct(RateMul), "ratemul")^0 *
-                lpeg.Cg(Behavior, "behavior")^0
-                )
-
-Path = LBrack * (Value)^0 * RBrack * Space
---Line = lpeg.Ct((Space * lpeg.Cg(Symbol, "symbol") * Space))^1 * Null
+Path = generate_path_grammar(symtab)
 Line = Path * Null * Space
 Line = lpeg.Ct(Line)
 Lines = lpeg.Ct(Line^0)
