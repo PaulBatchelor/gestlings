0c37f2bf0c375569ed9a061064fce9b37a201ef3
paul <paul@noemail.net>
2023-05-01 18:51:52 +0000

    moved path grammar to separate file
    FossilOrigin-Name: e8add74497db3dc4f0973b354a92e6b340a19f745eef3feb654703455d826801

--- path/notate.lua
+++ path/notate.lua
@@ -4,6 +4,7 @@ msgpack = dofile("../util/MessagePack.lua")
 base64 = dofile("../util/base64.lua")
 asset = dofile("../asset/asset.lua")
 asset = asset:new({msgpack=msgpack, base64=base64})
+path_grammar = dofile("grammar.lua")
 
 function generate_symtab(symbols)
     local symtab = {}
@@ -97,80 +98,19 @@ end
 hexstr = generate_hexstring(symtab, lines)
 print(hexstr)
 
-function generate_path_grammar(symtab)
+function generate_grammar(pathgram)
     local Space = lpeg.S(" \n\t")^0
-    local LBrack = lpeg.P(string.format("%02x", symtab["bracket_left"]))
-    local RBrack = lpeg.P(string.format("%02x", symtab["bracket_right"]))
-    local One = lpeg.P(string.format("%02x", symtab["one"]))
-    local Two = lpeg.P(string.format("%02x", symtab["two"]))
-    local Three = lpeg.P(string.format("%02x", symtab["three"]))
-    local Four = lpeg.P(string.format("%02x", symtab["four"]))
-    local Five = lpeg.P(string.format("%02x", symtab["five"]))
-    local Six = lpeg.P(string.format("%02x", symtab["six"]))
-    local Seven = lpeg.P(string.format("%02x", symtab["seven"]))
-    local Eight = lpeg.P(string.format("%02x", symtab["eight"]))
-    local Nine = lpeg.P(string.format("%02x", symtab["nine"]))
-    local Ten = lpeg.P(string.format("%02x", symtab["ten"]))
-    local Eleven = lpeg.P(string.format("%02x", symtab["eleven"]))
-    local Twelve = lpeg.P(string.format("%02x", symtab["twelve"]))
-    local Thirteen = lpeg.P(string.format("%02x", symtab["thirteen"]))
-    local Fourteen = lpeg.P(string.format("%02x", symtab["fourteen"]))
-    local Fifteen = lpeg.P(string.format("%02x", symtab["fifteen"]))
-    local Zero = lpeg.P(string.format("%02x", symtab["zero"]))
-    local RateMulStart = lpeg.P(string.format("%02x", symtab["ratemulstart"]))
-    local RateMulEnd = lpeg.P(string.format("%02x", symtab["ratemulend"]))
-    local Divider = lpeg.P(string.format("%02x", symtab["divider"]))
-    local Linear = lpeg.P(string.format("%02x", symtab["linear"]))
-    local Step = lpeg.P(string.format("%02x", symtab["step"]))
-    local GlissBig = lpeg.P(string.format("%02x", symtab["gliss_big"]))
-    local GlissMedium = lpeg.P(string.format("%02x", symtab["gliss_medium"]))
-    local GlissSmall = lpeg.P(string.format("%02x", symtab["gliss_small"]))
-
-    local Symbol = LBrack + RBrack + One + Two + Three + Four
-    local Number =
-        Zero / "0" +
-        One / "1" +
-        Two / "2" +
-        Three / "3" +
-        Four / "4" +
-        Five / "5" +
-        Six / "6" +
-        Seven / "7" +
-        Eight / "8" +
-        Nine / "9" +
-        Ten / "a" +
-        Eleven / "b" +
-        Twelve / "c" +
-        Thirteen / "d" +
-        Fourteen / "e" +
-        Fifteen / "f"
-    local Behavior = Space * (
-        Linear / "linear" +
-        Step / "step" +
-        GlissBig / "gliss_large" +
-        GlissMedium / "gliss_medium" +
-        GlissSmall / "gliss_small"
-        ) * Space
-    local Nibble = Space * Number * Space
-    local Div = (Space * Divider * Space)^0
-    local Hex = Div * lpeg.Ct(Nibble*Nibble) * Div
-    local RateMul = RateMulStart * Hex * Div * Hex * RateMulEnd
-    local Value = lpeg.Ct(lpeg.Cg(Hex, "value") *
-                    lpeg.Cg(lpeg.Ct(RateMul), "ratemul")^0 *
-                    lpeg.Cg(Behavior, "behavior")^0
-                    )
-
-    local Path = LBrack * (Value)^0 * RBrack * Space
-    return Path
+    local Null = lpeg.P("00")
+    local Line = pathgram * Null * Space
+    Line = lpeg.Ct(Line)
+    local Lines = lpeg.Ct(Line^0)
+    return Lines
 end
 
-Space = lpeg.S(" \n\t")^0
-Null = lpeg.P("00")
+Path = path_grammar.generate(symtab)
 
-Path = generate_path_grammar(symtab)
-Line = Path * Null * Space
-Line = lpeg.Ct(Line)
-Lines = lpeg.Ct(Line^0)
+Grammar = generate_grammar(Path)
+
 behaviors = {
     linear = 0,
     step = 1,
@@ -181,8 +121,6 @@ behaviors = {
 
 function parse(Lines, hexstr)
     local t = lpeg.match(Lines, hexstr)
-
-    -- pprint.pprint(t[2])
 
     local ratemul = {1, 1}
     local behavior = behaviors["linear"]
@@ -212,5 +150,5 @@ function parse(Lines, hexstr)
     return gpath
 end
 
-gpath = parse(Lines, hexstr)
+gpath = parse(Grammar, hexstr)
 asset:save(gpath, "path.bin.txt")
