06fb0634567ed8dc62e72ddab5e31e166b5f764e
paul <paul@noemail.net>
2023-05-07 19:18:53 +0000

    updates to path grammar
    FossilOrigin-Name: dcc3801dc68ec0a52329544768dd8d5e11f5b1128903ddd2e22d5a889a108a54

--- test/t/path_grammar.lua
+++ test/t/path_grammar.lua
@@ -37,6 +37,16 @@ fourteen, fifteen,
 bracket_right
 }
 
+proportional_gesture_path = {
+bracket_left,
+zero, zero,
+ratemulstart, one, one, ratemulend, linear,
+divider,
+fifteen, fifteen,
+ratemulstart, three, three, ratemulend, step,
+bracket_right
+}
+
 function test_path(symtab, gpath)
     local hexstr = symtools.hexstring(symtab, gpath)
 
@@ -48,10 +58,15 @@ function test_path(symtab, gpath)
             table.insert(out, v.value[2])
         end
         if v.ratemul ~= nil then
-            table.insert(out, v.ratemul[1][1])
-            table.insert(out, v.ratemul[1][2])
-            table.insert(out, v.ratemul[2][1])
-            table.insert(out, v.ratemul[2][2])
+            if #v.ratemul == 2 then
+                table.insert(out, v.ratemul[1][1])
+                table.insert(out, v.ratemul[1][2])
+                table.insert(out, v.ratemul[2][1])
+                table.insert(out, v.ratemul[2][2])
+            elseif #v.ratemul == 1 then
+                table.insert(out, v.ratemul[1][1])
+                table.insert(out, v.ratemul[1][2])
+            end
         end
 
         if v.behavior ~= nil then
@@ -62,24 +77,45 @@ function test_path(symtab, gpath)
     return out
 end
 
+function compare(out, ref)
+    local err = 0
+    if out == ref then
+        if verbose then
+            -- print("Everything is okay!")
+        end
+        err = 0
+    else
+        if verbose then
+            print("Test strings do not match:")
+            print("ref: " .. ref)
+            print("out: " .. out)
+        end
+        err = 1
+    end
+    return err
+end
+
+verbose = os.getenv("VERBOSE")
+verbose = (verbose ~= nil and verbose == "1")
+
 ref=
 "012345linear678703step898899gliss_largea"..
 "bfbabgliss_mediumcd12cdgliss_smallef"
 
-out = test_path(symtab, gesture_path)
+prop_ref= "0011linearff33step"
 
-verbose = os.getenv("VERBOSE")
-verbose = verbose ~= nil and verbose == "1"
-if out == ref then
+
+err = 0
+
+out = test_path(symtab, gesture_path)
+err = err + compare(out, ref)
+
+prop_out = test_path(symtab, proportional_gesture_path)
+err = err + compare(prop_out, prop_ref)
+
+if err > 0 then
     if verbose then
-        print("Everything is okay!")
-    end
-    os.exit(0)
-else
-    if verbose then
-        print("Test strings do not match:")
-        print("ref: " .. ref)
-        print("out: " .. out)
+        print(string.format("%d test(s) failed", err))
     end
     os.exit(1)
 end
