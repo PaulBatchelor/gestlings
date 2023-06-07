ec31c67eb523e85c2612de432c97808fe04ceb7c
paul <paul@noemail.net>
2023-06-07 20:55:59 +0000

    added initial goblins render script
    FossilOrigin-Name: b9d12b101de3c3a123b6910f66039546e5f4ce2432b69b9db35ed2a1a2a2bda6

---
+++ goblins/render.lua
@@ -0,0 +1,104 @@
+pprint = require("util/pprint")
+
+function goblin_render(name, dur)
+    gestku = require("gestku/" .. name)
+
+    os.execute("mkdir -p res/goblins")
+
+    if dur == nil then
+        dur = 11
+    end
+
+    gestku.patch()
+    lil(string.format(
+            "dup; wavouts zz zz %s.wav; computes %g",
+            name, dur))
+
+    sym = gestku.symbol()
+
+    nl = string.byte("\n");
+    block = string.byte("#");
+    space = string.byte("-");
+
+    tmp = {}
+    lines = {}
+
+    for s in string.gmatch(sym, ".")
+    do
+        if (string.byte(s) == nl) then
+            if #tmp > 0 then
+                table.insert(lines, tmp)
+                tmp = {}
+            end
+        elseif string.byte(s) == block then
+            table.insert(tmp, 1)
+        elseif string.byte(s) == space then
+            table.insert(tmp, 0)
+        end
+    end
+
+    nrows = #lines
+    ncols = #lines[1]
+    sz = 16
+    border = 12
+    width = ncols * sz + border*2
+    height = nrows * sz + border*2
+
+    lil(string.format("bpnew bp %d %d", width, height))
+    lil(string.format("bpset [grab bp] 0 %d %d %d %d",
+        border, border, width - 2*border, height - 2*border))
+
+    lil(string.format("bpset [grab bp] 1 0 0 %d %d", width, height))
+
+    for y,row in pairs(lines) do
+        color = 1
+        for x, color in pairs(row) do
+            lil(string.format(
+            "bprectf [bpget [grab bp] 0] %d %d %d %d %d",
+            (x - 1)*sz, (y - 1)*sz, sz, sz, color
+            ))
+        end
+    end
+
+    pixspace = 4
+    lil(string.format("bprect [bpget [grab bp] 1] %d %d %d %d 1",
+        border - pixspace, border - pixspace,
+        width - 2*border + 2*pixspace,
+        height - 2*border + 2*pixspace))
+
+    lil(string.format("bppbm [grab bp] %s.pbm", name))
+    ffmpeg_cmd = {
+        "ffmpeg",
+        "-y",
+        "-loop", "1",
+        "-i", name .. ".pbm",
+        "-i", name .. ".wav",
+        "-c:v", "libx264",
+        "-tune", "stillimage",
+        "-c:a", "aac", "-b:a", "256k",
+        "-pix_fmt", "yuv420p", "-shortest", "res/goblins/" .. name .. ".mp4"
+    }
+
+    imagemagick_cmd = {
+        "convert", name .. ".pbm", "res/goblins/" .. name .. ".png"
+    }
+
+    os.execute(table.concat(ffmpeg_cmd, " "))
+    os.execute(table.concat(imagemagick_cmd, " "))
+    os.remove(name .. ".pbm")
+    os.remove(name .. ".wav")
+end
+
+goblin_render("2023_01_23", 11)
+mnoreset()
+goblin_render("2023_01_24", 11)
+mnoreset()
+goblin_render("2023_01_25", 11)
+mnoreset()
+goblin_render("2023_01_26", 11)
+mnoreset()
+goblin_render("2023_01_27", 11)
+mnoreset()
+goblin_render("2023_01_28", 15)
+mnoreset()
+goblin_render("2023_01_29", 11)
