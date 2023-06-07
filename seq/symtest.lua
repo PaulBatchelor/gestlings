522a6fa2936f9ee9cdd161b9fc9e42111d1c991a
paul <paul@noemail.net>
2023-06-07 19:12:40 +0000

    updated seq symbolset
    FossilOrigin-Name: 4a18532833116ff7e1ba1bea90f3bde745c0952dceab6696fc5cdb9284d7bf31

--- seq/symtest.lua
+++ seq/symtest.lua
@@ -11,11 +11,15 @@ symtab = asset:load("test_syms_tab.b64")
 symtools.vars(symtab)()
 lil("bpnew bp 320 200")
 lil("bufnew buf 256")
-lil("bpset [grab bp] 0 10 10 300 180")
+margin = 50
+lil(string.format("bpset [grab bp] 0 %d %d %d %d",
+    margin, margin, 320 - 2*margin, 200 - 2*margin))
 lil("grab buf")
 buf = pop()
 lil("uf2load syms test_syms.uf2")
 lil("uf2load chicago ../fonts/chicago12.uf2")
+
+-- lil("bpoutline [bpget [grab bp] 0] 1")
 
 bytes = {}
 
@@ -26,10 +30,13 @@ function tcat(dst, src)
 end
 
 tokens = {
-    seq_val8, seq_val7, seq_val6, seq_val5, seq_val4,
-    seq_val3, seq_val2, seq_val1, seq_val0, seq_end,
-    seq_val16, seq_val15, seq_val14, seq_val13, seq_val12,
-    seq_val11, seq_val10, seq_val9,
+    seq_val1, seq_val2, seq_val3, seq_val4, seq_val5,
+    seq_val6, seq_val7, seq_val8, seq_val0, seq_end,
+    seq_val9, seq_val10, seq_val11, seq_val12, seq_val13,
+    seq_val14, seq_val15, seq_val16,
+    seq_end,
+    seq_dur1, seq_dur2, seq_dur3, seq_dur4,
+    seq_dur5, seq_dur6, seq_dur7, seq_dur8,
     seq_end,
     seq_val8, seq_dur1, seq_val1, seq_dur2, seq_val9, seq_dur3,
     seq_val15, seq_dur4, seq_val11, seq_dur5,
