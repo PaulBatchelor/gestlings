52bc71f5f4f0f7230db312e5f5e4207caf045caf
paul <paul@noemail.net>
2023-06-10 20:46:04 +0000

    goblang: some initial morphemes
    FossilOrigin-Name: 3acc4e9b47f66d7b0d106af072846789dcb3a4c238584691e71daae72b1cf303

--- goblo/generate_morphemes.lua
+++ goblo/generate_morphemes.lua
@@ -1,29 +1,63 @@
 gestku = require("gestku/gestku")
 warble = require("warble/warble")
 
-s16 = gestku.seq.seqfun(gestku.morpho)
-gest16 = gestku.gest.gest16fun(gestku.sr, gestku.core)
 G = gestku:new()
 
-head = {
-    gate = function(words)
-        gestku.tal.interpolate(words, 0)
-    end,
-    aspgt = function(words)
-        gestku.tal.interpolate(words, 0)
+function multimerge(merge, A, P)
+    for _, B in pairs(P) do
+        A = merge(A, B)
     end
+    return A
+end
 
-}
-
-function mkseq()
+function mkseq(seq, morpho)
     local m = G.morpheme
+    local s16 = seq.seqfun(morpho)
+    local mm = function(A, P)
+        return multimerge(m.merge, A, P)
+    end
 
     pm_pitch_upslide = {
         pitch = s16("a/ o"),
     }
 
+    pm_pitch_losigh = {
+        pitch = s16("b4/ a1~"),
+    }
+
+    pm_pitch_huhlo = {
+        pitch = s16("a4/ c1~"),
+    }
+
+    pm_pitch_hisigh = {
+        pitch = s16("h4/ c1~"),
+    }
+
+    pm_pitch_wambo = {
+        pitch = s16("f1/ d2 f1 d2 a3 b1~"),
+    }
+
+    pm_pitch_hambo = {
+        pitch = s16("n1/ j2 n1 j2 c3 d1~"),
+    }
+
+    pm_pitch_chippy = {
+        pitch = s16("o1/ a4~ o1/ a4~ o1/ a4~ o1/ a4~"),
+        -- pitch = s16("o1~"),
+        gate = s16("o1- o o o o o o o"),
+        ampdur = s16("a/ c~"),
+        ampatk = s16("b_"),
+        amprel = s16("n_"),
+        amp = s16("a3_ a1~"),
+        gain = s16("o2~ b1~ a1/"),
+    }
+
     pm_timbre_upslide = {
         timbre = s16("a/ o"),
+    }
+
+    pm_timbre_sharpdown = {
+        timbre = s16("o~ a"),
     }
 
     pm_fm_flat = {
@@ -38,7 +72,7 @@ function mkseq()
     }
 
     pm_amp_full = {
-        amp = s16("o"),
+        amp = s16("o3_ o1~"),
     }
 
     pm_gate_none = {
@@ -46,10 +80,17 @@ function mkseq()
     }
 
     pm_asp_default = {
-        aspdur = s16("h_"),
+        aspdur = s16("h3_ h1/"),
         aspgt = s16("a_"),
-        aspfreq = s16("b_"),
-        aspamt = s16("b_"),
+        aspfreq = s16("e3_ e1/"),
+        aspamt = s16("b3_ b1/"),
+    }
+
+    pm_asp_breathy = {
+        aspdur = s16("a3_ a1/"),
+        aspgt = s16("a3_"),
+        aspamt = s16("i3/ j1~"),
+        aspfreq = s16("f1/ a2"),
     }
 
     pm_amp_default = {
@@ -59,11 +100,11 @@ function mkseq()
     }
 
     pm_gain_full = {
-        gain = s16("o_"),
+        gain = s16("o3_ o1/"),
     }
 
     pm_gain_silence = {
-        gain = s16("a_"),
+        gain = s16("a2_ a1~"),
     }
 
     local mother = {}
@@ -82,47 +123,46 @@ function mkseq()
 
     default = mother{}
 
-    A = default
-    S = m.merge(default, pm_gain_silence)
+    local A = default
+    B = mm(A, {pm_asp_breathy, pm_pitch_losigh})
+    C = mm(B, {pm_pitch_hisigh})
+    D = mm(default, {pm_pitch_chippy})
+    E = mm(D, {pm_pitch_hisigh})
+    F = mm(D, {pm_pitch_huhlo})
+    H = mm(A, {pm_pitch_huhlo, pm_timbre_sharpdown})
+    I = mm(A, {pm_pitch_wambo, pm_timbre_sharpdown})
+    J = mm(A, {pm_pitch_hambo, pm_timbre_sharpdown, pm_asp_breathy})
+    local S = m.merge(default, pm_gain_silence)
 
     vocab = {
+        S = S,
         A = A,
-        S = S
+        B = B,
+        C = C,
+        D = D,
+        E = E,
+        F = F,
+        H = H,
+        I = I,
+        J = J,
     }
-    SEQ = "2[A]2(S)"
+
+
+    --SEQ = "D2(S)DA2[C]2(S)2[B]2(S)"
+    SEQ = "JSDSESFSHSIS"
     SEQ = gestku.mseq.parse(SEQ, vocab)
     return SEQ
 end
 
-function G:sound()
-    local lvl = gestku.core.liln
-    local sr = gestku.sr
+function mechanism(sr, core, gst, cnd)
     local pn = sr.paramnode
+    local lvl = core.liln
+    local param = core.paramf
     local ln = sr.node
-    local sig = gestku.sig
-    local cnd = sig:new()
-    local gst = G.gest
-    local param = gestku.core.paramf
 
-    ln(sr.phasor) {
-        rate = 15 / 10
-    }
+    gest16 = gestku.gest.gest16fun(sr, core)
 
-    cnd:hold()
-
-    local seq = mkseq()
-
-    G.words = {}
-	G.tal.start(G.words)
-
-	G.morpheme.articulate(G.path,
-	    G.tal, G.words, seq, head)
-
-    G.gest:compile(G.words)
-
-	gst:swapper()
-
-    fg = gest16(gst, "pitch", cnd, 60, 72)
+    fg = gest16(gst, "pitch", cnd, 48, 72)
     tg = gest16(gst, "timbre", cnd, 0.0, 10)
     vdepth  = gest16(gst, "vdepth", cnd, 0.0, 0.8)
     vrate = gest16(gst, "vrate", cnd, 4, 8)
@@ -153,7 +193,7 @@ function G:sound()
 
     asp_freq = pn(sr.mtof) {
         input = pn(sr.add) {
-            a = 60,
+            a = 50,
             b = gest16(gst, "aspfreq", cnd, 20, 60),
         }
     }
@@ -190,7 +230,7 @@ function G:sound()
             rel = param(0.3),
             gain = param(1.5),
             bw = param(200),
-            freq = param(200),
+            freq = asp_freq,
             val = asp_amt,
         },
     }
@@ -204,9 +244,48 @@ function G:sound()
     }
 
     lil("mul zz zz")
+end
 
-	gst:done()
+function G:sound()
+    local lvl = gestku.core.liln
+    local sr = gestku.sr
+    local pn = sr.paramnode
+    local ln = sr.node
+    local sig = gestku.sig
+    local cnd = sig:new()
+    local gst = G.gest
+    local param = gestku.core.paramf
+
+    ln(sr.phasor) {
+        rate = 15 / 10
+    }
+
+    cnd:hold()
+
+    local seq = mkseq(gestku.seq, gestku.morpho)
+
+    G.words = {}
+	G.tal.start(G.words)
+
+    head = {
+        gate = function(words)
+            gestku.tal.interpolate(words, 0)
+        end,
+        aspgt = function(words)
+            gestku.tal.interpolate(words, 0)
+        end
+
+    }
+
+
+	G.morpheme.articulate(G.path,
+	    G.tal, G.words, seq, head)
+    G.gest:compile(G.words)
+
+	gst:swapper()
+    mechanism(sr, gestku.core, gst, cnd)
     cnd:unhold()
+	gst:done()
 
 
     -- lil("dup; butlp zz 1000; softclip zz 2; add zz zz")
