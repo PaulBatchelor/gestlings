75b722e63fc079aa42dcd23d8f4426e2bcaaf63f
paul <paul@noemail.net>
2023-06-11 01:35:46 +0000

    initial constructs to get path synced with morpheme sequence
    FossilOrigin-Name: ccee453a7bf9e384cbf5255b5ef41bb97f907cc4198815312eae7b53dfd46def

--- goblo/generate_morphemes.lua
+++ goblo/generate_morphemes.lua
@@ -1,5 +1,7 @@
 gestku = require("gestku/gestku")
 warble = require("warble/warble")
+pp = require("util/pprint")
+json = require("util/json")
 
 G = gestku:new()
 
@@ -163,6 +165,14 @@ function mechanism(sr, core, gst, cnd)
     gest16 = gestku.gest.gest16fun(sr, core)
 
     fg = gest16(gst, "pitch", cnd, 48, 72)
+
+    global_pitch = gest16(gst, "gpitch", cnd, -12, 12)
+
+    pitch_biased = pn(sr.add) {
+        a = global_pitch,
+        b = fg,
+    }
+
     tg = gest16(gst, "timbre", cnd, 0.0, 10)
     vdepth  = gest16(gst, "vdepth", cnd, 0.0, 0.8)
     vrate = gest16(gst, "vrate", cnd, 4, 8)
@@ -201,7 +211,8 @@ function mechanism(sr, core, gst, cnd)
     asp_amt = gest16(gst, "aspamt", cnd, 0, 1)
 
     local g = warble.graph {
-        pitch = fg,
+        -- pitch = fg,
+        pitch = pitch_biased,
         mi = tg,
         fdbk = fdbk,
         amp = {
@@ -246,6 +257,86 @@ function mechanism(sr, core, gst, cnd)
     lil("mul zz zz")
 end
 
+function gcd(m, n)
+    while n ~= 0 do
+        local q = m
+        m = n
+        n = q % n
+    end
+    return m
+end
+
+function lcm(m, n)
+    return (m ~= 0 and n ~= 0) and
+        m * n / gcd(m, n) or 0
+end
+
+function add_fraction(a, b)
+    if a[2] == 0 then return b end
+    if b[2] == 0 then return a end
+    local s = lcm(a[2], b[2])
+    local as = s / a[2]
+    local bs = s / b[2]
+    return {as*a[1] + bs*b[1], s}
+end
+
+function reduce(a)
+    out = a
+    local s = gcd(out[1], out[2])
+
+    if (s ~= 0) then
+        out[1] = out[1] / s
+        out[2] = out[2] / s
+    end
+
+    return out
+end
+
+function fracmul(a, b)
+    local out = {a[1]*b[1], a[2]*b[2]}
+
+    return reduce(out)
+end
+
+function morphseq_dur(mseq)
+    local total = {0, 0}
+    for _, m in pairs(mseq) do
+        local r = m[2]
+        total = add_fraction(total, r)
+    end
+    -- r is a ratemultiplier against a normalize
+    -- path with dur 1. 2/1 is 2x faster, or dur 1/2.
+    -- inverse to get duration
+    -- this can be multiplied with normalized path
+    -- to stretch/squash it out
+    return {total[2], total[1]}
+end
+
+function path_normalizer(p)
+    local total = 0
+
+    for _, v in pairs(p) do
+        total = total + v[2]
+    end
+
+    return {total, 1}
+end
+
+function apply_ratemul(p, r, vertexer)
+    path_with_ratemul = {}
+
+    for _,v in pairs(p) do
+        local v_rm = {
+            v[1],
+            reduce({r[1], v[2]*r[2]}),
+            v[3]
+        }
+        table.insert(path_with_ratemul, vertexer(v_rm))
+    end
+
+    return path_with_ratemul
+end
+
 function G:sound()
     local lvl = gestku.core.liln
     local sr = gestku.sr
@@ -278,8 +369,29 @@ function G:sound()
     }
 
 
+    seqdur = morphseq_dur(seq)
+    -- pp(seqdur)
+    local s16 = gestku.seq.seqfun(gestku.morpho)
+    global_pitch = s16("a1/ h4~ o1/")
+    -- pp(global_pitch)
+    pnorm = path_normalizer(global_pitch)
+    -- pp(pnorm)
+    total_ratemul =fracmul(pnorm, seqdur)
+    -- pp(total_ratemul)
+    global_pitch_rescaled = apply_ratemul(global_pitch, total_ratemul, G.path.vertex)
+    -- pp(global_pitch_rescaled)
 	G.morpheme.articulate(G.path,
 	    G.tal, G.words, seq, head)
+
+    label = "gpitch"
+    G.tal.label(G.words, label)
+    if head[label] ~= nil then
+        head[label](G.words)
+    end
+    G.path.path(G.tal, G.words, global_pitch_rescaled)
+    G.tal.jump(G.words, label)
+
+    -- pp(G.words)
     G.gest:compile(G.words)
 
 	gst:swapper()
