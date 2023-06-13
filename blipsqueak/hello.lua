25fd0102ba2e5d91c14e7b0dbc9d0bc0bc2e7162
paul <paul@noemail.net>
2023-06-13 18:01:29 +0000

    blipsqueak: removed global imports, used components system instead
    FossilOrigin-Name: dc75fb4fa747797ad7e497185a06924a30779ff18808054f2a7ae2aeb20ae366

--- blipsqueak/hello.lua
+++ blipsqueak/hello.lua
@@ -1,20 +1,3 @@
-warble = require("warble/warble")
-
-msgpack = require("util/MessagePack")
-base64 = require("util/base64")
-asset = require("asset/asset")
-asset = asset:new({msgpack=msgpack, base64=base64})
-
-core = require("util/core")
-sigrunes = require("sigrunes/sigrunes")
-gest = require("gest/gest")
-tal = require("tal/tal")
-morpheme = require("morpheme/morpheme")
-path = require("path/path")
-seq = require("seq/seq")
-morpho = require("morpheme/morpho")
-sig = require("sig/sig")
-
 pprint = require("util/pprint")
 
 function resolve(seq, lookup)
@@ -39,14 +22,7 @@ function mcat(tab)
     return o
 end
 
-function mkseq(seq)
-    vocab = asset:load("blipsqueak/morphemes.b64")
-    words = asset:load("blipsqueak/words.b64")
-
-    SEQ = mcat {
-        words.HELLO, words.IAM, words.PLEASED, words.WELCOME
-    }
-    SEQ = resolve(SEQ, vocab)
+function mkseq(phrase)
     return SEQ
 end
 
@@ -68,61 +44,151 @@ function mkconductor(sig, sr, rate)
     return cnd
 end
 
-function sound()
-    local lvl = core.liln
-    local sr = sigrunes
+function load_component(comp, top_path, name, filepath)
+    if comp[name] == nil then
+        comp[name] = dofile(top_path .. filepath .. ".lua")
+        return true
+    end
+
+    return false
+end
+
+function load_blipsqueak_components(comp, top_path)
+    top_path = top_path or ""
+    comp = comp or {}
+
+    load_component(comp, top_path, "core", "util/core")
+    load_component(comp, top_path, "sigrunes", "sigrunes/sigrunes")
+    load_component(comp, top_path, "gest", "gest/gest")
+    load_component(comp, top_path, "morpheme", "morpheme/morpheme")
+    load_component(comp, top_path, "path", "path/path")
+    load_component(comp, top_path, "seq", "seq/seq")
+    load_component(comp, top_path, "morpho", "morpheme/morpho")
+    load_component(comp, top_path, "tal", "tal/tal")
+    load_component(comp, top_path, "sig", "sig/sig")
+    load_component(comp, top_path, "warble", "warble/warble")
+    load_component(comp, top_path, "msgpack", "util/MessagePack")
+    load_component(comp, top_path, "base64", "util/base64")
+    local loaded = load_component(comp, top_path, "asset", "asset/asset")
+
+    if loaded then
+        comp.asset = comp.asset:new {
+            msgpack = comp.msgpack,
+            base64 = comp.base64
+        }
+    end
+
+    return comp
+end
+
+function blipsqueak_components(o)
+    local comp = {}
+    o = o or {}
+    comp.sigrunes = o.sigrunes or sigrunes
+    assert(comp.sigrunes ~= nil, "sigrunes not found")
+    comp.core = o.core or core
+    assert(comp.core ~= nil, "core not found")
+    comp.tal = o.tal or tal
+    assert(comp.tal ~= nil, "tal not found")
+    comp.gest = o.gest or gest
+    assert(comp.gest ~= nil, "gest not found")
+    comp.asset = o.asset or asset
+    assert(comp.asset ~= nil, "asset not found")
+    comp.morpho = o.morpho or morpho
+    assert(comp.morpho ~= nil, "morpho not found")
+    comp.seq = o.seq or seq
+    assert(comp.seq ~= nil, "seq not found")
+    comp.sig = o.sig or sig
+    assert(comp.sig ~= nil, "sig not found")
+    comp.path = o.path or path
+    assert(comp.path ~= nil, "path not found")
+    comp.warble = o.warble or warble 
+    assert(comp.warble ~= nil, "warble not found")
+    comp.morpheme = o.morpheme or morpheme
+    assert(comp.morpheme ~= nil, "morpheme not found")
+    comp.msgpack = o.msgpack or msgpack
+    assert(comp.msgpack ~= nil, "msgpack not found")
+    comp.base64 = o.base64 or base64
+    assert(comp.msgpack ~= nil, "base64 not found")
+    comp.asset = o.asset or asset
+    assert(comp.msgpack ~= nil, "asset not found")
+
+    return comp
+end
+
+function blipsqueak_speak(comp, phrase, pitchseq, temposeq)
+    local lvl = comp.core.liln
+    local sr = comp.sigrunes
     local pn = sr.paramnode
     local ln = sr.node
-    local cnd = sig:new()
-    local gst = gest:new{tal=tal}
-    local param = core.paramf
+    local cnd = comp.sig:new()
+    local gst = comp.gest:new{tal=comp.tal}
+    local param = comp.core.paramf
     local words = {}
 
     gst:create()
-    local mseq = mkseq(seq)
+
+    vocab = comp.asset:load("blipsqueak/morphemes.b64")
+    words = comp.asset:load("blipsqueak/words.b64")
+
+    phrase_morphs = {}
+
+    for _, wrd in pairs(phrase) do
+        table.insert(phrase_morphs, words[wrd])
+    end
+
+    SEQ = mcat(phrase_morphs)
+    mseq = resolve(SEQ, vocab)
 
     words = {}
-	tal.start(words)
+	comp.tal.start(words)
 
     head = {
         gate = function(words)
-            tal.interpolate(words, 0)
+            comp.tal.interpolate(words, 0)
         end,
         aspgt = function(words)
-            tal.interpolate(words, 0)
+            comp.tal.interpolate(words, 0)
         end
 
     }
 
-    local s16 = seq.seqfun(morpho)
+    local s16 = comp.seq.seqfun(comp.morpho)
 
-    global_pitch = s16("h1/ k2~ h1/ d h i2~ h4_")
-    global_tempo = s16("d1/ f d4 c")
+    global_pitch = s16(pitchseq)
+    global_tempo = s16(temposeq)
 
-    create_aligned_path(path, tal, words, mseq, global_pitch, "gpitch")
-    create_aligned_path(path, tal, words, mseq, global_tempo, "gtempo")
+    create_aligned_path(comp.path, comp.tal, words, mseq, global_pitch, "gpitch")
+    create_aligned_path(comp.path, comp.tal, words, mseq, global_tempo, "gtempo")
 
-	morpheme.articulate(path, tal, words, mseq, head)
+	comp.morpheme.articulate(comp.path, comp.tal, words, mseq, head)
 
     gst:compile(words)
 
 	gst:swapper()
 
-    cnd = mkconductor(sig, sr, 14/10)
+    cnd = mkconductor(comp.sig, sr, 14/10)
 
-    lines = asset:load("blipsqueak/mechanism.b64")
+    lines = comp.asset:load("blipsqueak/mechanism.b64")
 
     ext = {
         cnd = cnd.reg
     }
 
-    -- TODO: dynamically select/mark free registers based
-    -- on data contents
-
-    free = {7, 8}
     info = lines.info
 
-    lines = core.apply_register_macros(lines.patch, info, free, ext)
+    -- dynamically select/mark free registers based on number
+    -- of setters
+    free = {}
+
+    for _=1,#info.setters do
+        lil("param [regnxt 0]")
+        local r = pop()
+        lil("regmrk " .. r)
+        table.insert(free, r)
+    end
+
+    lines = comp.core.apply_register_macros(lines.patch, info, free, ext)
     for _, l in pairs(lines) do
         if type(l) == "string" then
             error("expected table structure: '" .. l .. "'")
@@ -133,7 +199,14 @@ function sound()
 
     cnd:unhold()
 	gst:done()
+end
 
+function sound()
+    comp = blipsqueak_components(load_blipsqueak_components())
+    phrase = {"HELLO", "IAM", "PLEASED", "WELCOME"}
+    pitchseq = "h1/ k2~ h1/ d h i2~ h4_"
+    temposeq = "d1/ f d4 c"
+    blipsqueak_speak(comp, phrase, pitchseq, temposeq)
     lil("mul zz [dblin -6]")
     lil([[
 dup; dup;
