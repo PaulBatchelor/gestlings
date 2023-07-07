82f2d8e301c2a1348ba33c97f2a342a232379388
paul <paul@noemail.net>
2023-07-07 21:13:19 +0000

    zetdoing. blocky6
    FossilOrigin-Name: deb49360d68595be570eda2966f969da4d977903ff414d00a98b8499cd94a965

---
+++ scratch/blocky6.lua
@@ -0,0 +1,97 @@
+uf2 = require("path/uf2")
+font = {}
+function add_to_font(sym)
+    font[sym.id] = sym
+end
+
+lbrack = {
+    id = 0x01,
+    width = 5,
+    name = "lbrack",
+    shortcut = "",
+    bits = {
+
+        "####",
+        "####",
+        "##--",
+        "##--",
+        "####",
+        "####",
+        "----",
+        "----",
+        "----",
+        "-","-","-","-",
+        "-","-","-",
+    }
+}
+add_to_font(lbrack)
+
+rbrack = {
+    id = 0x02,
+    width = 5,
+    name = "rbrack",
+    shortcut = "",
+    bits = {
+
+        "####",
+        "####",
+        "--##",
+        "--##",
+        "####",
+        "####",
+        "----",
+        "----",
+        "----",
+        "-","-","-","-",
+        "-","-","-",
+    }
+}
+add_to_font(rbrack)
+
+stick = {
+    id = 0x03,
+    width = 3,
+    name = "stick",
+    shortcut = "",
+    bits = {
+
+        "##",
+        "##",
+        "##",
+        "##",
+        "##",
+        "##",
+
+        "--",
+        "--",
+        "--",
+        "-","-","-","-",
+        "-","-","-",
+    }
+}
+add_to_font(stick)
+
+dot = {
+    id = 0x04,
+    width = 3,
+    name = "dot",
+    shortcut = "",
+    bits = {
+
+        "--",
+        "--",
+        "##",
+        "##",
+        "--",
+        "--",
+
+        "--",
+        "--",
+        "--",
+        "-","-","-","-",
+        "-","-","-",
+    }
+}
+add_to_font(dot)
+
+uf2.generate(font, "scratch/blocky6.uf2")
