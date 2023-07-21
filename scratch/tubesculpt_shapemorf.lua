0f2086ec294b035e4aaec7d2e5ee2165b2f81dd3
paul <paul@noemail.net>
2023-07-21 16:50:38 +0000

    sound/visual demo for tubesculpt/shapemorf
    FossilOrigin-Name: 095e6a33221e2a1a4d847565bf5109a9450bbef75693795b8b6e0729cf0d95b1

--- scratch/tubesculpt_shapemorf.lua
+++ scratch/tubesculpt_shapemorf.lua
@@ -1,3 +1,5 @@
+-- produces a "mantra", a looping setting of tract shapes
+
 gest = require("gest/gest")
 pprint = require("util/pprint")
 tal = require("tal/tal")
@@ -14,15 +16,34 @@ for k,v in pairs(lookup) do
 end
 
 gm = gest.behavior.gliss_medium
+gl = gest.behavior.gliss
 lin = gest.behavior.linear
 
-os.exit()
+shapes = {
+    "2b1d8a",
+    "4e8a8e",
+    "83ae8a",
+    "172828",
+    "54f27d",
+    "8abe8d",
+}
+
 vt = path.vertex
 test_path = {
-    vt{"oo", {1, 1}, gm},
-    vt{"ah", {1, 1}, gm},
-    vt{"oo", {1, 1}, lin},
-    vt{"ah", {1, 1}, lin}
+    vt{shapes[1], {1, 1}, gm},
+    vt{shapes[2], {1, 1}, gm},
+    vt{shapes[1], {1, 1}, lin},
+    vt{shapes[2], {1, 1}, gl},
+
+    vt{shapes[3], {1, 1}, gm},
+    vt{shapes[4], {1, 1}, gm},
+    vt{shapes[3], {1, 1}, lin},
+    vt{shapes[4], {1, 1}, gl},
+
+    vt{shapes[5], {1, 1}, gm},
+    vt{shapes[6], {1, 1}, gm},
+    vt{shapes[5], {1, 1}, lin},
+    vt{shapes[6], {1, 1}, gl}
 }
 
 words = {}
@@ -33,14 +54,14 @@ tal.begin(words)
 tal.label(words, "vowshapes")
 path.path(tal, words, test_path, lookup)
 tal.jump(words, "vowshapes")
-pprint(words)
+-- pprint(words)
 
 g = gest:new{tal = tal}
 g:create()
 g:compile(words)
 
 program = {
-"tubularnew 17.0 -1",
+"tubularnew 20.0 -1",
 "regset zz 0",
 -- "tabnew [tubularsz [regget 0]]",
 -- "regset zz 1",
@@ -53,22 +74,28 @@ program = {
 table.concat({
     -- gvm, lut, tubular, program , conductor
     "shapemorf",
-    g:get(),                                -- gestvm getter
-    "[grab lut]",                           -- LUT
-    "[regget 0]",                           -- tubular
-    "[" .. g:gmemsymstr("vowshapes") .. "]",-- program
-    "[phasor 1 0]"                          -- conductor
+    g:get(),
+    "[grab lut]",
+    "[regget 0]",
+    "[" .. g:gmemsymstr("vowshapes") .. "]",
+    "[phasor [scale [expmap [flipper [phasor 0.05 0]] 3] 1 10] 0]"
 }, " "),
 
 "regget 0",
-"param 80",
+"param 30",
+"jitseg 0.3 -0.3 0.5 2 1",
+"add zz zz",
+"mtof zz",
 "param 0.3",
 "param 0.1",
 "param 0.0",
 "glot zz zz zz zz",
 "tubular zz zz zz",
-"butlp zz 3000",
-"mul zz 0.8",
+"butlp zz 3000", "mul zz [dblin -3]",
+"dup", "dup",
+"bigverb zz zz 0.9 10000",
+"drop", "dcblocker zz", "mul zz [dblin -16]",
+"add zz zz",
 "dup",
 "wavouts zz zz test.wav",
 }
@@ -77,6 +104,68 @@ for _, line in pairs(program) do
     lil(line)
 end
 
-lil("computes 10")
+lil("computes 60")
 
--- shapemorf.del(lut)
+function lilt(tab)
+    lil(table.concat(tab, " "))
+end
+
+spacing = 16
+square_size = 8
+line_width = 0
+-- for i=1,#shapes do
+--     line_width = line_width + #shapes[i]
+-- end
+
+-- line_width =
+--     (line_width * square_size) +
+--     (#shapes - 1) * (2 * square_size)
+
+line_height =
+    4*#shapes*square_size +
+    (#shapes - 1) * (2 * square_size) + spacing*2
+
+    print(line_height)
+-- characters happen to be all fixed width of 6 tiles
+line_width = 6*square_size + spacing*2
+
+canvas_width = 240
+canvas_height = 320
+
+lilt{"bpnew", "bp", canvas_width, canvas_height}
+lilt{"bpset",
+    "[grab bp]", 0,
+    spacing + (canvas_width - line_width)//2,
+    spacing + (canvas_height - line_height)//2,
+    line_width - spacing,
+    line_height - spacing}
+lilt{"bpset", "[grab bp]", 1, 0, 0, canvas_width, canvas_height}
+
+-- lilt{"bpline", "[bpget [grab bp] 1]",
+-- canvas_width // 2, 0, canvas_width//2, canvas_height, 1}
+
+function draw_shape(shp, square_size, yoff)
+    for c=1,#shp do
+        col=string.byte(shp, c)
+        col=tonumber(string.char(col), 16)
+        for row=1,4 do
+            local s = col & (1 << (row -1))
+            if (s > 0) then
+                lil(table.concat({
+                    "bprectf",
+                    "[bpget [grab bp] 0]",
+                    (c - 1)*square_size, (row - 1)*square_size+yoff,
+                    square_size, square_size, 1
+                }, " "))
+            end
+        end
+    end
+end
+
+xoff = 0
+for i=1,#shapes do
+    draw_shape(shapes[i], square_size, xoff)
+    xoff=xoff + 6*square_size
+end
+
+lil("bppng [grab bp] scratch/tubesculpt_shapemorf.png")
