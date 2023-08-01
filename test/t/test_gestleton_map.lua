eefd517ce995ab8b7777853bc9b13ac8dd6780b2
paul <paul@noemail.net>
2023-08-01 21:17:13 +0000

    more tilemap work
    FossilOrigin-Name: a0a8a52a8b4ed227a003757208dcdaf30fc7e878e1674310f70f92a1a91fc543

--- test/t/test_gestleton_map.lua
+++ test/t/test_gestleton_map.lua
@@ -15,6 +15,36 @@ end
 
 lilt{"bpnew", "bp", 320, 240}
 lilt{"bpset", "[grab bp]", 0, 0, 0, 320, 240}
+
+lil("bpget [grab bp] 0")
+reg = pop()
+
+tile = tilemap[1]
+
+function draw_tile(tile, xpos, ypos)
+    ypos = ypos * 8
+    xpos = xpos * 8
+    for y = 1, 8 do
+        for x = 1, 8 do
+            local row = tile[y]
+            local val =
+                row &
+                ((1 << 2*(x - 1)) |
+                 (1 << 2*(x - 1) + 1))
+            val = val >> (2*(x - 1))
+            local s= 0
+
+            if val == 1 then s = 1 end
+            btprnt.draw(reg, xpos + x, ypos + y, s)
+        end
+    end
+end
+
+draw_tile(tilemap[1], 1, 1)
+draw_tile(tilemap[2], 2, 1)
+draw_tile(tilemap[3], 1, 2)
+draw_tile(tilemap[4], 2, 2)
+
 lilt{"bppng", "[grab bp]", "test.png"}
 
 
