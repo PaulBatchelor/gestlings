982e3728178844d93e4e4a9968c093a1a7545e7c
paul <paul@noemail.net>
2023-08-01 17:02:59 +0000

    some initial work on mapgen
    FossilOrigin-Name: cc5282fac8bf3ead2fb38750f9594345149e9fb86e502ff702655874ddf53321

--- test/t/test_gestleton_map.lua
+++ test/t/test_gestleton_map.lua
@@ -1,5 +1,21 @@
 gmapgen = require("levels/gestleton/mapgen")
+asset = require("asset/asset")
+asset = asset.instantiate()
+pprint = require("util/pprint")
 
 map = gmapgen:new()
+
+tilemap = asset:load("levels/gestleton/tiles.b64")
+
+pprint(tilemap[1])
+
+function lilt(tab)
+    lil(table.concat(tab, " "))
+end
+
+lilt{"bpnew", "bp", 320, 240}
+lilt{"bpset", "[grab bp]", 0, 0, 0, 320, 240}
+lilt{"bppng", "[grab bp]", "test.png"}
+
 
 map:draw()
