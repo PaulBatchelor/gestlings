d6dceb34e221315f0af08e7d7e0355573a440f2e
paul <paul@noemail.net>
2023-08-08 16:47:16 +0000

    working mouth animation
    FossilOrigin-Name: 7fde0cdc043e5b3925137df23a4ab9990c37c76301f9940a9ae67ab3d26c6d41

--- avatar/mouth/mouthanim.lua
+++ avatar/mouth/mouthanim.lua
@@ -82,13 +82,248 @@ mouth1 = {
     }
 }
 
-generate_program(syms, program)
-apply_mouth_shape(vm, mouth1)
+mouth2 = {
+    circleness = 0.1,
+    roundedge = 0.1,
+    circrad = 0.7,
+    points = {
+        {-0.1, 0.5},
+        {-0.5, -0.5},
+        {0.5, -0.5},
+        {0.1, 0.5},
+    }
+}
 
-function frame(framenum)
+mouth1b = {
+    circleness = 0.8,
+    roundedge = 0.1,
+    circrad = 0.7,
+    points = {
+        {-0.5, 0.5},
+        {-0.1, -0.5},
+        {0.1, -0.5},
+        {0.5, 0.5},
+    }
+}
+
+mouth2b = {
+    circleness = 0.8,
+    roundedge = 0.1,
+    circrad = 0.7,
+    points = {
+        {-0.1, 0.5},
+        {-0.5, -0.5},
+        {0.5, -0.5},
+        {0.1, 0.5},
+    }
+}
+
+mouth3 = {
+    circleness = 0.0,
+    roundedge = 0.08,
+    circrad = 0.7,
+    points = {
+        {-0.5, 0.02},
+        {-0.5, -0.02},
+        {0.5, -0.02},
+        {0.5, 0.02},
+    }
+}
+
+mouth3b = {
+    circleness = 0.1,
+    roundedge = 0.08,
+    circrad = 0.7,
+    points = {
+        {-0.5, 0.02},
+        {-0.5, -0.02},
+        {0.5, -0.02},
+        {0.5, 0.02},
+    }
+}
+
+mouth4 = {
+    circleness = 0.0,
+    roundedge = 0.08,
+    circrad = 0.7,
+    points = {
+        {-0.2, 0.6},
+        {-0.02, -0.6},
+        {0.02, -0.6},
+        {0.2, 0.6},
+    }
+}
+
+mouth4b = {
+    circleness = 0.3,
+    roundedge = 0.08,
+    circrad = 0.7,
+    points = {
+        {-0.2, 0.6},
+        {-0.02, -0.6},
+        {0.02, -0.6},
+        {0.2, 0.6},
+    }
+}
+
+mouth5 = {
+    circleness = 1.0,
+    roundedge = 0.08,
+    circrad = 0.2,
+    points = {
+        {-0.5, 0.5},
+        {-0.1, -0.5},
+        {0.1, -0.5},
+        {0.5, 0.5},
+    }
+}
+
+mouth1c = {
+    circleness = 0.0,
+    roundedge = 0.0,
+    circrad = 0.7,
+    points = {
+        {-0.5, 0.5},
+        {-0.1, -0.5},
+        {0.1, -0.5},
+        {0.5, 0.5},
+    }
+}
+
+mouth2c = {
+    circleness = 0.0,
+    roundedge = 0.0,
+    circrad = 0.7,
+    points = {
+        {-0.1, 0.5},
+        {-0.5, -0.5},
+        {0.5, -0.5},
+        {0.1, 0.5},
+    }
+}
+
+mouth6 = {
+    circleness = 0.0,
+    roundedge = 0.0,
+    circrad = 0.7,
+    points = {
+        {-0.7, 0.7},
+        {-0.4, -0.4},
+        {0.4, -0.5},
+        {0.5, 0.5},
+    }
+}
+
+shearx = 0.2
+mouth7 = {
+    circleness = 0.1,
+    roundedge = 0.05,
+    circrad = 0.7,
+    points = {
+        {-0.3 + shearx, 0.5},
+        {-0.3 - shearx, -0.5},
+        {0.3 - shearx, -0.5},
+        {0.3 + shearx, 0.5},
+    }
+}
+
+shearx = 0.5
+mouth7b = {
+    circleness = 0.0,
+    roundedge = 0.1,
+    circrad = 0.7,
+    points = {
+        {-0.3 - shearx, 0.5},
+        {-0.3 + shearx, -0.5},
+        {0.3 + shearx, -0.5},
+        {0.3 - shearx, 0.5},
+    }
+}
+
+mouth2d = {
+    circleness = 0.1,
+    roundedge = 0.1,
+    circrad = 0.7,
+    points = {
+        {-0.1, 0.5},
+        {-0.8, 0.3},
+        {0.8, 0.3},
+        {0.1, 0.5},
+    }
+}
+
+mouth1d = {
+    circleness = 0.1,
+    roundedge = 0.1,
+    circrad = 0.7,
+    points = {
+        {-0.8, 0.5},
+        {-0.1, 0.3},
+        {0.1, 0.3},
+        {0.8, 0.5},
+    }
+}
+
+mouths = {
+    mouth1, mouth2, mouth1b, mouth2b,
+    mouth3, mouth3b, mouth4, mouth4b,
+    mouth5, mouth1c, mouth2c, mouth6,
+    mouth7, mouth7b, mouth2d, mouth1d
+}
+
+function mouth_interp(m1, m2, pos)
+    local newmouth = {}
+
+    newmouth.circleness =
+        pos*m2.circleness +
+        (1 - pos)*m1.circleness
+
+    newmouth.roundedge =
+        pos*m2.roundedge +
+        (1 - pos)*m1.roundedge
+
+    newmouth.circrad =
+        pos*m2.circrad +
+        (1 - pos)*m1.circrad
+
+    newmouth.points = {}
+    for i=1,4 do
+        newmouth.points[i] = {}
+        newmouth.points[i][1] =
+            pos*m2.points[i][1] +
+            (1 - pos)*m1.points[i][1]
+        newmouth.points[i][2] =
+            pos*m2.points[i][2] +
+            (1 - pos)*m1.points[i][2]
+    end
+
+    return newmouth
+end
+
+generate_program(syms, program)
+
+function gliss(a)
+    if (a < 0.5) then
+        a = 0
+    else
+        a = a - 0.5
+        if (a < 0) then a  = 0 end
+        a = a / 0.5
+        a = a * a * a
+    end
+
+    return a
+end
+
+function frame(fs)
+    framenum = fs.framenum
     if (framenum % 60 == 0) then
         print(framenum)
     end
+    local m1 = mouths[fs.current_mouth]
+    local m2 = mouths[fs.next_mouth]
+    local ms = mouth_interp(m1, m2, gliss(fs.pos))
+    apply_mouth_shape(vm, ms)
     lil("bpfill [bpget [grab bp] 0] 0")
     lil("grab gfx")
     lil("gfxfill 1")
@@ -98,13 +333,32 @@ function frame(framenum)
     lil("dup; gfxtransfer; gfxappend")
 end
 
-for i = 1, 300  do
-    frame(i)
+frame_state = {
+    pos = 0,
+    framenum = 0,
+    next_mouth = 2,
+    current_mouth = 1
+}
+
+for i = 1, 600  do
+    frame_state.framenum = i
+    frame(frame_state)
+    frame_state.pos = frame_state.pos + (1/60)*2
+    if frame_state.pos > 1 then
+        frame_state.pos = frame_state.pos - 1
+        frame_state.current_mouth = frame_state.next_mouth
+        frame_state.next_mouth = (frame_state.next_mouth + 1)
+        if frame_state.next_mouth > #mouths then
+            frame_state.next_mouth = 1
+        end
+        print(frame_state.next_mouth, frame_state.current_mouth)
+    end
 end
 
 lil([[
 grab gfx
 gfxclose
-gfxmp4 tmp/mouthanim.h264 res/mouthanim.mp4
+gfxmp4 tmp/mouthanim.h264 tmp/mouthanim.mp4
 ]])
+os.execute("ffmpeg -y -i tmp/mouthanim.mp4 -pix_fmt yuv420p res/mouthanim.mp4")
 -- lil("bppng [grab bp] res/sdfvm_mouth.png")
