ac15f520643e1429d69c5933f61c6c3f47f6bd43
paul <paul@noemail.net>
2023-08-08 15:45:24 +0000

    mouthanim: initial file
    FossilOrigin-Name: fc97355b00542588ddb0da2f36f6baa49be00aeb3db10260f0ed73e003617ce2

--- avatar/mouth/mouthanim.lua
+++ avatar/mouth/mouthanim.lua
@@ -1,27 +1,13 @@
-#+TITLE: A mouthlike-shape
-
-@!(img "/res/sdfvm_mouth.png")!@
-
-A proof of concept lua program that generates an SDFVM
-shader program in lua, then renders it to a lua btprnt
-image (see above).
-
-Builds off of @!(ref "mouthtests")!@. Note how lua
-abstractions make for less code. Neater code maybe? But
-definitely less code.
-
-#+NAME: sdfvm_mouth.lua
-#+BEGIN_SRC lua :tangle avatar/mouth/sdfvm_mouth.lua
 json = require("util/json")
 pprint = require("util/pprint")
 
+os.execute("mkdir -p tmp res")
 fp = io.open("avatar/sdfvm_lookup_table.json")
 syms = json.decode(fp:read("*all"))
 fp:close()
 
--- pprint(syms)
-
 lil("bpnew bp 256 256")
+lil("gfxnew gfx 256 256")
 lil("bpset [grab bp] 0 0 0 256 256")
 lil("bufnew buf 256")
 lil("grab buf")
@@ -29,6 +15,14 @@ program = pop()
 lil("sdfvmnew vm")
 lil("grab vm")
 vm = pop()
+
+lil("grab gfx")
+lil("dup")
+lil("gfxopen tmp/mouthanim.h264")
+
+lil("gfxclrset 1 1.0 1.0 1.0")
+lil("gfxclrset 0 0.0 0.0 0.0")
+lil("drop")
 
 function generate_program(syms, program)
    mnobuf.append(program, syms.point)
@@ -91,6 +85,26 @@ mouth1 = {
 generate_program(syms, program)
 apply_mouth_shape(vm, mouth1)
 
-lil("bpsdf [bpget [grab bp] 0] [grab vm] [grab buf]")
-lil("bppng [grab bp] res/sdfvm_mouth.png")
-#+END_SRC
+function frame(framenum)
+    if (framenum % 60 == 0) then
+        print(framenum)
+    end
+    lil("bpfill [bpget [grab bp] 0] 0")
+    lil("grab gfx")
+    lil("gfxfill 1")
+    lil("bpsdf [bpget [grab bp] 0] [grab vm] [grab buf]")
+    lil("dup")
+    lil("bptr [grab bp] 0 0 256 256 0 0 0")
+    lil("dup; gfxtransfer; gfxappend")
+end
+
+for i = 1, 300  do
+    frame(i)
+end
+
+lil([[
+grab gfx
+gfxclose
+gfxmp4 tmp/mouthanim.h264 res/mouthanim.mp4
+]])
+-- lil("bppng [grab bp] res/sdfvm_mouth.png")
