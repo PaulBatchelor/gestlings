18f39b198f585de57c3d37b17497f6d5c2c2b3c6
paul <paul@noemail.net>
2023-08-15 19:47:18 +0000

    more tweaks to ritual music
    FossilOrigin-Name: f0d49277e49172c235eea96c573adbab1ad08cd6402c32edb2e5ee20bdb67bc6

--- scratch/ritual_music.lua
+++ scratch/ritual_music.lua
@@ -3,6 +3,8 @@ gest = require("gest/gest")
 pprint = require("util/pprint")
 tal = require("tal/tal")
 path = require("path/path")
+sig = require("sig/sig")
+sigrunes = require("sigrunes/sigrunes")
 
 lilts = core.lilts
 zz = "zz"
@@ -13,8 +15,10 @@ lut = pop()
 lookup = shapemorf.generate_lookup(lut)
 
 gm = gest.behavior.gliss_medium
+step = gest.behavior.step
 gl = gest.behavior.gliss
 lin = gest.behavior.linear
+g50 = gest.behavior.gate_50
 
 shapes = {
     "2b1d8a",
@@ -26,22 +30,203 @@ shapes = {
 }
 
 vt = path.vertex
-test_path = {
-    vt{shapes[1], {1, 1}, gm},
-    vt{shapes[2], {1, 1}, gm},
-    vt{shapes[1], {1, 1}, lin},
-    vt{shapes[2], {1, 1}, gl},
 
-    vt{shapes[3], {1, 1}, gm},
-    vt{shapes[4], {1, 1}, gm},
-    vt{shapes[3], {1, 1}, lin},
-    vt{shapes[4], {1, 1}, gl},
+function generate_chant()
+    test_path = {
+        vt{shapes[1], {1, 3}, gm},
 
-    vt{shapes[5], {1, 1}, gm},
-    vt{shapes[6], {1, 1}, gm},
-    vt{shapes[5], {1, 1}, lin},
-    vt{shapes[6], {1, 1}, gl}
+        vt{shapes[2], {1, 2}, gm},
+        vt{shapes[3], {1, 2}, gm},
+
+        vt{shapes[6], {1, 4}, lin},
+
+
+        vt{shapes[4], {1, 3}, gm},
+    }
+
+    chant_pitch = {
+        vt{12+2, {1, 3}, gm},
+
+        vt{12+2, {1, 8}, lin},
+
+        vt{12+3, {1, 3}, gm},
+
+        vt{12, {1, 3}, gm},
+
+        vt{12, {1, 2}, lin},
+        vt{12, {1, 2}, lin},
+
+        vt{12 - 3, {1, 4}, lin},
+        vt{12 - 3, {1, 3}, gm},
+    }
+    return test_path, chant_pitch
+end
+
+test_path, chang_pitch = generate_chant()
+
+bell_strike = {
+    vt{1, {1, 14}, g50},
 }
+
+vox_gate = {
+    vt{0, {1, 3}, step},
+    vt{1, {1, 8}, step},
+    vt{0, {1, 3}, step},
+}
+
+breakdown = {
+    vt{0, {1, 14*6}, lin},
+    vt{1, {1, 14}, step},
+}
+
+function chant(gst, cnd)
+    lil("param [regnxt -1]")
+    local r_tubular = pop()
+    lilts {
+        {"tubularnew", 20.0, -1},
+        {"regset", zz, r_tubular},
+
+        {
+            -- gvm, lut, tubular, program , conductor
+            "shapemorf",
+            g:get(),
+            "[grab lut]",
+            "[regget " .. r_tubular .. "]",
+            "[" .. g:gmemsymstr("vowshapes") .. "]",
+            "[" .. table.concat(cnd:getstr(), " ") .. "]"
+        },
+    }
+
+    lilts {
+        {"regget", r_tubular},
+    }
+
+    gesture(sigrunes, gst, "chantpitch", cnd)
+
+    lilts {
+        {"param", 30 + 7 - 12},
+        {"add", zz, zz},
+        {"jitseg", 0.3, -0.3, 0.5, 2, 1},
+        {"add", zz, zz},
+        {"mtof", zz},
+        {"param", 0.3},
+        {"param", 0.1},
+        {"param", 0.0},
+        {"glot", zz, zz, zz, zz},
+    }
+
+    gesture(sigrunes, gst, "voxgate", cnd)
+
+    lilts {
+        {"envar", zz, 0.1, 0.2},
+        {"mul", zz, zz},
+        {"tubular", zz, zz, zz},
+        {"butlp", zz, 8000},
+        -- {"lowshelf", zz, 80, 2, 0.5},
+        {"mul", zz, "[dblin -3]"},
+    }
+end
+
+function static()
+    lilts {
+        {"chaosnoise", 1.8, 100, 0.1},
+        {"buthp", zz, 100},
+        {"highshelf", zz, 4000, 8, 0.5},
+        {"mul", zz, "[dblin 3]"},
+    }
+end
+
+function gesture(sr, gst, name, cnd)
+    sr.node(gst:node()){
+        name = name,
+        conductor = core.liln(cnd:getstr())
+    }
+end
+
+function bell(gst, cnd)
+    gesture(sigrunes, gst, "bell", cnd)
+    lilts {
+        {"gtick", zz},
+        {"tgate", zz, 0.003},
+        {"smoother", zz, 0.005},
+        -- {"noise"},
+        -- {"butlp", zz, 300},
+        -- {"butlp", zz, 300},
+        -- {"mul", zz, zz},
+        {"dup"},
+
+        {"modalres", zz, 400*0.8, 20*0.1},
+        {"swap"},
+        {"modalres", zz, 450, 20},
+        {"add", zz, zz},
+        {"mul", zz, "[dblin 3]"},
+        -- {"limit", zz, -0.5, 0.5},
+        {"dcblocker", zz},
+        {"buthp", zz, 60},
+
+        {"dup"},
+        -- -- s m
+        {"modalres", zz, 300, 200*0.5},
+        {"mul", zz, "[dblin -30]"},
+        -- m s
+        {"swap"},
+        -- m s s
+        {"dup"},
+        -- m s m
+        {"modalres", zz, 880, 880*0.5},
+        {"mul", zz, "[dblin -16]"},
+        -- m m s
+        {"swap"},
+        -- m m m
+        {"modalres", zz, 880*1.321, 880*0.5},
+        {"mul", zz, "[dblin -15]"},
+        {"add", zz, zz},
+        {"add", zz, zz},
+
+        {"dcblocker", zz},
+        -- {"mul", zz, "[dblin -18]"},
+        {"limit", zz, -0.9, 0.9},
+    }
+end
+
+function reverb(gst, cnd)
+    local brk = sig:new()
+    lilts {
+        {"dup"},
+    }
+    gesture(sigrunes, gst, "breakdown", cnd)
+    brk:hold()
+
+    brk:get()
+    lilts {
+        {"expmap", zz, 8},
+        {"scale", zz, 1, 50},
+        {"softclip", zz, zz},
+        {"dup"},
+        {"bigverb", zz, zz, 0.97, 10000},
+        {"drop"},
+        {"dcblocker", zz},
+        {"mul", zz, "[dblin -10]"},
+    }
+
+    -- lilts {
+    --     {"add", 0, 1},
+    --     {"sine", 10, 1},
+    --     {"biscale", zz, 0, 1}
+    -- }
+    -- -- brk:get()
+    -- lilts {
+    --     {"add", 0, 1},
+    --     {"expmap", zz, -5},
+    --     {"crossfade", zz, zz, zz},
+    --     {"mul", zz, zz}
+    -- }
+
+    lilts {
+        {"add", zz, zz},
+    }
+    brk:unhold()
+end
 
 words = {}
 tal.begin(words)
@@ -50,98 +235,42 @@ tal.label(words, "vowshapes")
 path.path(tal, words, test_path, lookup)
 tal.jump(words, "vowshapes")
 
+tal.label(words, "bell")
+tal.interpolate(words, 0)
+path.path(tal, words, bell_strike)
+tal.jump(words, "bell")
+
+tal.label(words, "voxgate")
+path.path(tal, words, vox_gate)
+tal.jump(words, "voxgate")
+
+tal.label(words, "chantpitch")
+path.path(tal, words, chant_pitch)
+tal.jump(words, "chantpitch")
+
+tal.label(words, "breakdown")
+path.path(tal, words, breakdown)
+tal.halt(words)
+
 g = gest:new{tal = tal}
 g:create()
 g:compile(words)
 
 lilts {
-    {"metro", (153/(60 * 14))},
-    {"tgate", zz, 0.005},
-    {"smoother", zz, 0.001},
-    {"dup"},
-    {"modalres", zz, 800, 40},
-    {"swap"},
-    {"modalres", zz, 1800, 1400},
-    {"add", zz, zz},
-    {"mul", zz, "[dblin -10]"},
-    {"limit", zz, -0.5, 0.5},
-    {"dcblocker", zz},
-    {"buthp", zz, 200},
-
-    {"dup"},
-    -- s m
-    {"modalres", zz, 300, 50},
-    -- m s
-    {"swap"},
-    -- m s s
-    {"dup"},
-    -- m s m
-    {"modalres", zz, 880, 200},
-    -- m m s
-    {"swap"},
-    -- m m m
-    {"modalres", zz, 880+100, 500},
-    {"add", zz, zz},
-    {"add", zz, zz},
-
-    {"dcblocker", zz},
-    {"mul", zz, "[dblin -18]"},
-    {"limit", zz, -0.9, 0.9},
-
-    {"chaosnoise", 1.8, 300, 0.1},
-    {"buthp", zz, 500},
-    {"highshelf", zz, 8000, 4, 0.5},
-    {"mul", zz, "[dblin -3]"},
-    {"add", zz, zz},
+    {"phasor", (153 / 60), 0},
 }
 
-lilts {
-    {"tubularnew", 20.0, -1},
-    {"regset", zz, 0},
+cnd = sig:new()
+cnd:hold()
 
-    {
-        -- gvm, lut, tubular, program , conductor
-        "shapemorf",
-        g:get(),
-        "[grab lut]",
-        "[regget 0]",
-        "[" .. g:gmemsymstr("vowshapes") .. "]",
-        "[phasor [scale [expmap [flipper [phasor 0.05 0]] 3] 1 10] 0]"
-    },
-
-    {"regget", 0},
-    {"param", 33},
-    {"jitseg", 0.3, -0.3, 0.5, 2, 1},
-    {"add", zz, zz},
-    {"mtof", zz},
-    {"param", 0.3},
-    {"param", 0.1},
-    {"param", 0.0},
-    {"glot", zz, zz, zz, zz},
-    {"tubular", zz, zz, zz},
-    {"butlp", zz, 8000},
-    {"lowshelf", zz, 80, 4, 0.5},
-    {"mul", zz, "[dblin -3]"},
-}
-
+bell(g, cnd)
+static()
 lil("add zz zz")
+chant(g, cnd)
+lil("add zz zz")
+reverb(g, cnd)
 
-lilts {
-    {"dup"},
-    {"phasor", 1/40, 0},
-    {"flipper", zz},
-    {"expmap", zz, 3},
-    {"scale", zz, 1, 40},
-    {"softclip", zz, zz},
-    {"dup"},
-    {"bigverb", zz, zz, 0.97, 10000},
-    {"drop"},
-    {"dcblocker", zz},
-    {"mul", zz, "[dblin -10]"},
-    {"add", zz, zz},
-}
-
-
+cnd:unhold()
 
 lilts {
     {"wavout", "zz", "test.wav"},
