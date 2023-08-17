b6c1ee60d39ce5eacc47afa805685758cd416705
paul <paul@noemail.net>
2023-08-17 15:43:54 +0000

    trixie, her code cleaner
    FossilOrigin-Name: 1efa22588e39344eb6fd0a233ccd66cc04f22e81c3c8607709059da0cd56c59c

--- scratch/squad.lua
+++ scratch/squad.lua
@@ -12,35 +12,6 @@ fp:close()
 -- pprint(syms)
 lilt = core.lilt
 
-lil("bpnew bp 640 480")
-gestling_size = 640 / 4
-
-gestling_yoff = (480 // 2) - (gestling_size  // 2)
--- it looks a little better with them raised a little bit
--- maybe the mouth wants to be more y-centered?
-gestling_yoff = gestling_yoff - (gestling_size // 8)
-
-for i=1,4 do
-    lilt {
-        "bpset",
-        "[grab bp]", i - 1,
-        (i - 1)*gestling_size, gestling_yoff,
-        gestling_size, gestling_size
-    }
-end
-lilt {
-    "bpset",
-    "[grab bp]", 4,
-    0, 0, 640, 480
-}
-
-lil("bufnew buf 256")
-lil("grab buf")
-program = pop()
-lil("sdfvmnew vm")
-lil("grab vm")
-vm = pop()
-
 function tokenize(s)
     local sep = lpeg.S(" \t\n")
     local elem = lpeg.C((1 - sep)^0)
@@ -48,48 +19,8 @@ function tokenize(s)
     return lpeg.match(p, s)
 end
 
-function generate_program(syms, bytebuf)
-    local script = {
-        "point vec2 0.45 -0.33 add2 scalar 0.37 circle",
-        "scalar 0 regset",
-        "scalar 0 regget",
-        "scalar 0.02 onion",
-        "point vec2 0.45 -0.33 add2 scalar 0.1 circle add",
-        "gtz",
-
-        "point vec2 -0.45 -0.33 add2 scalar 0.37 circle",
-        "scalar 1 regset",
-        "scalar 1 regget",
-        "scalar 0.02 onion",
-        "point vec2 -0.45 -0.33 add2 scalar 0.1 circle add",
-        "gtz",
-
-        "add",
-
-        "point scalar 0.75 circle scalar 0.02 onion",
-        "scalar 0 regget scalar 1 regget",
-        "add",
-        "swap subtract",
-        "gtz",
-        "add",
-
-        "point vec2 0 0.5 add2",
-        "scalar 0 uniform scalar 1 uniform",
-        "scalar 2 uniform scalar 3 uniform",
-        "poly4",
-        -- r5: rounded edge amount
-        "scalar 5 uniform roundness",
-        -- r6: rounded edge amount
-        "point vec2 0 0.5 add2",
-        "scalar 6 uniform circle",
-        -- r4: circleness amount
-        "scalar 4 uniform lerp gtz",
-
-        "add"
-    }
-
+function generate_bytecode(script, bytebuf)
     local program = tokenize(table.concat(script, "\n"))
-
 
     for _,p in pairs(program) do
         if #p == 0 then
@@ -118,6 +49,56 @@ function apply_mouth_shape(vm, mouth)
     end
 end
 
+function setup()
+    lil("bpnew bp 640 480")
+    gestling_size = 640 / 4
+
+    gestling_yoff = (480 // 2) - (gestling_size  // 2)
+    -- it looks a little better with them raised a little bit
+    -- maybe the mouth wants to be more y-centered?
+    gestling_yoff = gestling_yoff - (gestling_size // 8)
+
+    for i=1,4 do
+        lilt {
+            "bpset",
+            "[grab bp]", i - 1,
+            (i - 1)*gestling_size, gestling_yoff,
+            gestling_size, gestling_size
+        }
+    end
+    lilt {
+        "bpset",
+        "[grab bp]", 4,
+        0, 0, 640, 480
+    }
+end
+
+function draw(vm, singer)
+    apply_mouth_shape(vm, mouth1)
+    lilt {
+        "bpsdf",
+        string.format("[bpget [grab bp] %d]", singer.id),
+        "[grab vm]",
+        "[grab " .. singer.bufname .. "]"
+    }
+end
+
+function mksinger(vm, name, id)
+    local singer = {
+    }
+
+    singer.bufname = name
+    lilt {"bufnew", singer.bufname, 256}
+    lilt {"grab", singer.bufname}
+    singer.bytebuf = pop()
+    singer.id = id
+
+    return function(program)
+        generate_bytecode(program, singer.bytebuf)
+        return singer
+    end
+end
+
 mouth1 = {
     circleness = 0.1,
     roundedge = 0.1,
@@ -130,21 +111,50 @@ mouth1 = {
     }
 }
 
-generate_program(syms, program)
-apply_mouth_shape(vm, mouth1)
+setup()
+lil("sdfvmnew vm")
+lil("grab vm")
+vm = pop()
 
-for i=1,4 do
-    lilt {
-        "bpsdf",
-        string.format("[bpget [grab bp] %d]", i-1),
-        "[grab vm]",
-        "[grab buf]"
-    }
-end
+trixie = mksinger(vm, "trixie", 3) {
+    "point vec2 0.45 -0.33 add2 scalar 0.37 circle",
+    "scalar 0 regset",
+    "scalar 0 regget",
+    "scalar 0.02 onion",
+    "point vec2 0.45 -0.33 add2 scalar 0.1 circle add",
+    "gtz",
 
+    "point vec2 -0.45 -0.33 add2 scalar 0.37 circle",
+    "scalar 1 regset",
+    "scalar 1 regget",
+    "scalar 0.02 onion",
+    "point vec2 -0.45 -0.33 add2 scalar 0.1 circle add",
+    "gtz",
 
--- lilt {
---     "bpline", "[bpget [grab bp] 4]",
---     0, 480//2, 640, 480//2, 1
--- }
+    "add",
+
+    "point scalar 0.75 circle scalar 0.02 onion",
+    "scalar 0 regget scalar 1 regget",
+    "add",
+    "swap subtract",
+    "gtz",
+    "add",
+
+    "point vec2 0 0.5 add2",
+    "scalar 0 uniform scalar 1 uniform",
+    "scalar 2 uniform scalar 3 uniform",
+    "poly4",
+    -- r5: rounded edge amount
+    "scalar 5 uniform roundness",
+    -- r6: rounded edge amount
+    "point vec2 0 0.5 add2",
+    "scalar 6 uniform circle",
+    -- r4: circleness amount
+    "scalar 4 uniform lerp gtz",
+
+    "add"
+}
+
+draw(vm, trixie)
+
 lil("bppng [grab bp] scratch/squad.png")
