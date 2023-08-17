fde63ef0bcc26408a736951ae36c44be49a40660
paul <paul@noemail.net>
2023-08-17 17:20:53 +0000

    diamond is looking good
    FossilOrigin-Name: dbe127db2893cfb9ba582c50479ea98e3b166666c4d3ab62fa2b9cdf6ec31888

--- scratch/squad.lua
+++ scratch/squad.lua
@@ -20,7 +20,18 @@ function tokenize(s)
 end
 
 function generate_bytecode(script, bytebuf)
-    local program = tokenize(table.concat(script, "\n"))
+
+    input_script = {}
+
+    for _,line in pairs(script) do
+        if (type(line) == "table") then
+            table.insert(input_script, table.concat(line, " "))
+        else
+            table.insert(input_script, line)
+        end
+    end
+
+    local program = tokenize(table.concat(input_script, "\n"))
 
     for _,p in pairs(program) do
         if #p == 0 then
@@ -51,19 +62,29 @@ end
 
 function setup()
     lil("bpnew bp 640 480")
-    gestling_size = 640 / 4
+    gestling_width = 640 / 2
+    gestling_height = 480 / 2
 
-    gestling_yoff = (480 // 2) - (gestling_size  // 2)
+    gestling_yoff = (480 // 2) - (gestling_height // 2)
     -- it looks a little better with them raised a little bit
     -- maybe the mouth wants to be more y-centered?
-    gestling_yoff = gestling_yoff - (gestling_size // 8)
+    gestling_yoff = gestling_yoff - (gestling_width // 8)
+
+    local gw = gestling_width
+    local gh = gestling_height
+    local dims = {
+        {0, 0, gw, gh},
+        {gw, 0, gw, gh},
+        {0, gh, gw, gh},
+        {gw, gh, gw, gh},
+    }
 
     for i=1,4 do
         lilt {
             "bpset",
             "[grab bp]", i - 1,
-            (i - 1)*gestling_size, gestling_yoff,
-            gestling_size, gestling_size
+            dims[i][1], dims[i][2],
+            dims[i][3], dims[i][4]
         }
     end
     lilt {
@@ -116,48 +137,135 @@ lil("sdfvmnew vm")
 lil("grab vm")
 vm = pop()
 
-trixie = mksinger(vm, "trixie", 3) {
-    "point vec2 0.45 -0.33 add2 scalar 0.37 circle",
-    "scalar 0 regset",
-    "scalar 0 regget",
-    "scalar 0.02 onion",
-    "point vec2 0.45 -0.33 add2 scalar 0.1 circle add",
-    "gtz",
+function mktrixie(vm, id)
+    local scale = 0.6
+    return mksinger(vm, "trixie", id) {
+        {
+            "point",
+            "vec2", 0.45*scale, -0.33*scale, "add2",
+            "scalar", 0.4*scale, "circle"
+        },
+        "scalar 0 regset",
+        "scalar 0 regget",
+        {"scalar", 0.02*scale, "onion"},
+        {
+            "point",
+            "vec2", 0.45*scale, -0.33*scale, "add2",
+            "scalar", 0.15*scale, "circle",
+            "add"
+        },
+        "gtz",
 
-    "point vec2 -0.45 -0.33 add2 scalar 0.37 circle",
-    "scalar 1 regset",
-    "scalar 1 regget",
-    "scalar 0.02 onion",
-    "point vec2 -0.45 -0.33 add2 scalar 0.1 circle add",
-    "gtz",
+        {
+            "point",
+            "vec2", -0.45*scale, -0.33*scale, "add2",
+            "scalar", 0.4*scale, "circle"
+        },
+        "scalar 1 regset",
+        "scalar 1 regget",
+        {"scalar", 0.02*scale, "onion"},
+        {
+            "point",
+            "vec2", -0.45*scale, -0.33*scale, "add2",
+            "scalar", 0.15*scale, "circle",
+            "add"
+        },
+        "gtz",
 
-    "add",
+        "add",
 
-    --"point scalar 0.75 circle scalar 0.02 onion",
-    "point vec2 0.75 0.6 ellipse scalar 0.02 onion",
-    "scalar 0 regget scalar 1 regget",
-    "add",
-    "swap subtract",
-    "gtz",
-    "add",
+        "point",
+        {"vec2", 0.65*scale, 0.5*scale, "ellipse"},
+        {"scalar", 0.02*scale, "onion"},
+        "scalar 0 regget scalar 1 regget",
+        "add",
+        "swap subtract",
+        "add",
 
-    "point vec2 0 0.5 add2",
-    "scalar 0 uniform scalar 1 uniform",
-    "scalar 2 uniform scalar 3 uniform",
-    "poly4",
-    -- r5: rounded edge amount
-    "scalar 5 uniform roundness",
-    -- r6: rounded edge amount
-    "point vec2 0 0.5 add2",
-    "scalar 6 uniform circle",
-    -- r4: circleness amount
-    "scalar 4 uniform lerp gtz",
+        -- "point vec2 0 0.5 add2",
+        -- "scalar 0 uniform scalar 1 uniform",
+        -- "scalar 2 uniform scalar 3 uniform",
+        -- "poly4",
+        -- -- r5: rounded edge amount
+        -- "scalar 5 uniform roundness",
+        -- -- r6: rounded edge amount
+        -- "point vec2 0 0.5 add2",
+        -- "scalar 6 uniform circle",
+        -- -- r4: circleness amount
+        -- "scalar 4 uniform lerp gtz",
 
-    "add"
+        -- "add"
+        -- "point vec2 0.75 0.6 ellipse gtz",
+        "gtz",
+    }
+end
 
-    -- "point vec2 0.75 0.6 ellipse gtz",
-}
+function poly4(points, scale)
+    local shape = {}
+
+    for i =1,4 do
+        table.insert(shape, "vec2")
+        table.insert(shape, points[i][1]*scale)
+        table.insert(shape, points[i][2]*scale)
+    end
+
+    table.insert(shape, "poly4")
+    return shape
+end
+
+function mkdiamond(vm, id)
+    local scale = 0.6
+    return mksinger(vm, "diamond", id) {
+
+        {
+            "point",
+            "vec2", 1.0*scale, -0.2*scale, "add2",
+            "scalar", 0.2*scale, "circle"
+        },
+        {"scalar", 0.02*scale, "onion"},
+        {
+            "point",
+            "vec2", 1.0*scale, -0.2*scale, "add2",
+            "scalar", 0.08*scale, "circle",
+            "add"
+        },
+        "gtz",
+
+        {
+            "point",
+            "vec2", -1.0*scale, -0.2*scale, "add2",
+            "scalar", 0.2*scale, "circle"
+        },
+        {"scalar", 0.02*scale, "onion"},
+        {
+            "point",
+            "vec2", -1.0*scale, -0.2*scale, "add2",
+            "scalar", 0.08*scale, "circle",
+            "add"
+        },
+        "gtz add",
+
+        -- {"point", "vec2", 1.5*scale, 1.4*scale, "ellipse"},
+
+        "point",
+        poly4({
+            {-0.9, 1.0},
+            {-1.0, -1.0},
+            {1.0, -1.0},
+            {0.9, 1.0}
+        }, scale),
+        {"scalar", 0.5*scale, "roundness"},
+        --{"point", "vec2", 0.0, -0.5*scale, "add2", "scalar", 0.5*scale, "circle"},
+        -- {"scalar", 0.01*scale, "union_smooth"},
+        {"scalar", 0.02*scale, "onion", "gtz", "add"},
+    }
+end
+
+trixie = mktrixie(vm, 3)
+
+diamond = mkdiamond(vm, 0)
 
 draw(vm, trixie)
+draw(vm, diamond)
 
 lil("bppng [grab bp] scratch/squad.png")
