dcdd8e179769d405e270522271cee7a45ea1b0b5
paul <paul@noemail.net>
2023-09-01 23:30:32 +0000

    messagebox: initial operations and event handler skeleton
    FossilOrigin-Name: 44227febf7a369e40d35932fd5e64a3a89ff19f198f1f943a90cc69ca7a18d20

--- scratch/messagebox.lua
+++ scratch/messagebox.lua
@@ -5,20 +5,57 @@ descript = require("descript/descript")
 
 pprint = require("util/pprint")
 
-script = [[You are finally awake.
+messagebox = {}
+function messagebox.new()
+    local buf = {}
+
+    local lines = {}
+    for i=1,4 do lines[i] = "" end
+
+    buf.lines = lines
+    buf.linepos = 1
+    buf.font = "chicago"
+    return buf
+end
+
+function messagebox.append(buf, ch)
+    buf.lines[buf.linepos] =
+        buf.lines[buf.linepos] .. ch
+end
+function messagebox.newline(buf)
+    buf.linepos = buf.linepos + 1
+end
+
+function draw_textline(txt, ypos, font)
+    lilt {
+        "uf2txtln",
+        "[bpget [grab bp] 0]",
+        "[grab " .. font .. " ]",
+        0, ypos,
+        "\"" .. txt .. "\""
+    }
+end
+
+function messagebox.draw(buf, lheight)
+    for idx, line in pairs(buf.lines) do
+        draw_textline(line, (idx-1)*lheight, buf.font)
+    end
+end
+
+function messagebox.loadfont(fontname, filepath)
+    lilt {"uf2load", fontname, filepath}
+end
+
+script = [[You're finally awake.
 hello there!
 Welcome to Gestleton.
 City of the Gestlings.]]
 
 out = descript.parse(script)
 
-textbuf = {}
+buf = messagebox.new()
 
-for _, line in pairs(out[1]) do
-    table.insert(textbuf, line)
-end
-
-lilt {"uf2load", "chicago", "fonts/chicago12.uf2"}
+messagebox.loadfont("chicago", "fonts/chicago12.uf2")
 lilt {"bpnew", "bp", 200, 60}
 padding = 4
 lilt {
@@ -29,19 +66,71 @@ lilt {
     60 - 2*padding
 }
 
-function draw_textline(txt, ypos)
-    lilt {
-        "uf2txtln",
-        "[bpget [grab bp] 0]",
-        "[grab chicago]",
-        0, ypos,
-        "'" .. txt .. "'"
-    }
+-- messagebox.append(buf, "a")
+-- messagebox.append(buf, "b")
+-- messagebox.append(buf, "c")
+-- messagebox.append(buf, "d")
+-- messagebox.newline(buf)
+-- messagebox.append(buf, "h")
+-- messagebox.append(buf, "e")
+-- messagebox.append(buf, "l")
+-- messagebox.append(buf, "l")
+-- messagebox.append(buf, "o")
+
+function new_event(t, name, data)
+    return {t, name, data}
 end
 
-for idx, line in pairs(textbuf) do
-    draw_textline(line, (idx-1)*12)
+function append(events, t, ch)
+    table.insert(events, new_event(t, "append", ch))
 end
--- draw_textline(textbuf[1], 0)
--- draw_textline(textbuf[2], 12)
+
+function newline(events)
+    table.insert(events, new_event(t, "newline", nil))
+end
+
+events = {}
+
+t = 1
+rate = 5
+append(events, t, "a")
+t = t + rate
+append(events, t, "b")
+t = t + rate
+append(events, t, "c")
+t = t + rate
+append(events, t, "d")
+t = t + rate
+newline(events, t)
+append(events, t, "h")
+t = t + rate
+append(events, t, "e")
+t = t + rate
+append(events, t, "l")
+t = t + rate
+append(events, t, "l")
+t = t + rate
+append(events, t, "o")
+
+event_handler = {
+    append = function(mb, data)
+        messagebox.append(mb, data)
+    end,
+    newline = function(mb, data)
+        messagebox.newline(mb, data)
+    end,
+}
+
+for _,ev in pairs(events) do
+    local f = event_handler[ev[2]]
+
+    if f ~= nil then
+        f(buf, ev[3])
+    else
+        error("unknown event " .. ev[2])
+    end
+end
+
+messagebox.draw(buf, 12)
+
 lilt {"bppng", "[grab bp]", "scratch/messagebox.png"}
