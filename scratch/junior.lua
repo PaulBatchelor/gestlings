8b2a9bedb045ed4221b3a7e20fae7bb4e7db7d8b
paul <paul@noemail.net>
2023-09-08 00:21:39 +0000

    junior with inflections
    FossilOrigin-Name: d9d026174213b9416322ecb550b4da7c08c7c9d12fb28b6b1a644dd71895909c

--- scratch/junior.lua
+++ scratch/junior.lua
@@ -6,6 +6,8 @@ pprint = require("util/pprint")
 gest = require("gest/gest")
 path = require("path/path")
 tal = require("tal/tal")
+morpheme = require("morpheme/morpheme")
+sigrunes = require("sigrunes/sigrunes")
 
 function setup()
     lil("shapemorfnew lut shapes/junior.b64")
@@ -24,25 +26,84 @@ function setup()
     local gm = gest.behavior.gliss_medium
     local gl = gest.behavior.gliss
     local lin = gest.behavior.linear
+    local stp = gest.behavior.step
 
     local p_shapes = {}
 
-    table.insert(p_shapes, vtx{A, {1, 1}, lin})
-    table.insert(p_shapes, vtx{B, {1, 1}, gl})
-    table.insert(p_shapes, vtx{A, {1, 1}, lin})
-    table.insert(p_shapes, vtx{C, {1, 1}, gm})
-    table.insert(p_shapes, vtx{A, {1, 1}, lin})
-    table.insert(p_shapes, vtx{D, {1, 1}, gm})
-    table.insert(p_shapes, vtx{A, {1, 1}, lin})
-    table.insert(p_shapes, vtx{E, {1, 1}, gm})
+    pat_a = morpheme.template({
+        shapes = {
+            {A, 2, lin},
+            {B, 1, gl},
+            {C, 1, lin},
+            {D, 1, gm},
+            {E, 2, gm}
+        },
+
+        aspiration = {
+            {0xFF, 1, gm},
+            {0x0, 5, stp},
+        },
+
+        inflection = {
+            {0x0, 3, lin},
+        },
+
+        gate = {
+            {1, 5, stp},
+            {0, 1, stp},
+        }
+    })
+
+
+
+    local m = {}
+
+    morpheme.append(path, m, {1, 1}, pat_a{})
+    morpheme.append(path, m, {1, 1}, pat_a {
+        inflection = {
+            {0x0, 3, lin},
+            {0x4, 1, gm},
+        },
+    })
+    morpheme.append(path, m, {1, 1}, pat_a {
+        inflection = {
+            {0x4, 1, gl},
+            {0x0, 1, gl},
+            {0x2, 1, gm},
+        },
+    })
+
+    morpheme.append(path, m, {1, 1}, pat_a {
+        inflection = {
+            {0x4, 3, lin},
+            {0x0, 1, gm},
+        },
+    })
+
+    -- table.insert(p_shapes, vtx{A, {1, 1}, lin})
+    -- table.insert(p_shapes, vtx{B, {1, 1}, gl})
+    -- table.insert(p_shapes, vtx{A, {1, 1}, lin})
+    -- table.insert(p_shapes, vtx{C, {1, 1}, gm})
+    -- table.insert(p_shapes, vtx{A, {1, 1}, lin})
+    -- table.insert(p_shapes, vtx{D, {1, 1}, gm})
+    -- table.insert(p_shapes, vtx{A, {1, 1}, lin})
+    -- table.insert(p_shapes, vtx{E, {1, 1}, gm})
 
     local words = {}
     tal.begin(words)
+    morpheme.compile(tal, path, words, m, nil, lookup)
 
-    tal.label(words, "shapes")
-    path.path(tal, words, p_shapes, lookup)
-    tal.jump(words, "shapes")
+    -- tal.label(words, "shapes")
+    -- path.path(tal, words, p_shapes, lookup)
+    -- tal.jump(words, "shapes")
     return words
+end
+
+function gesture(sr, gst, name, cnd)
+    sr.node(gst:node()){
+        name = name,
+        conductor = core.liln(cnd:getstr())
+    }
 end
 
 function patch(words)
@@ -51,7 +112,7 @@ function patch(words)
     G:create()
     G:compile(words)
     lilts {
-        {"phasor", 1, 0},
+        {"phasor", 1.3, 0},
     }
 
     local cnd = sig:new()
@@ -74,22 +135,19 @@ function patch(words)
     }
 
     lilt {"regget", 4}
+    gesture(sigrunes, G, "inflection", cnd)
     lilts {
+        {"mul", "zz", 0.5},
         {"param", 60},
+        {"add", "zz", "zz"},
         -- {"sine", 7, 0.1},
         -- {"add", "zz", "zz"},
         {"mtof", "zz"},
         {"param", 0.2},
         {"param", 0.15},
         {"param", 0.1},
-        -- {"param", tract_effort[vid]},
-        -- {"param", breathiness[vid][1]},
-        -- {"param", breathiness[vid][2]},
         {"glot", "zz", "zz", "zz", "zz"}
-        -- {"blsaw", "zz"},
     }
-
-    -- lilt {"mul", "zz", 0}
 
     lilts {
         {"noise"},
@@ -113,8 +171,11 @@ function patch(words)
         {"mul", "zz", 0.5},
     }
 
+    gesture(sigrunes, G, "aspiration", cnd)
     lilts {
-        {"crossfade", "zz", "zz", 0.0}
+        {"mul", "zz", 1.0 / 255.0},
+        {"smoother", "zz", "0.005"},
+        {"crossfade", "zz", "zz", "zz"}
     }
 
     lilts {
@@ -124,14 +185,14 @@ function patch(words)
         {"regclr", 4},
     }
 
-    -- gesture(sigrunes, gst, gate_label, local_cnd)
+    gesture(sigrunes, G, "gate", cnd)
     -- lil("gestvmlast " .. gst:get())
     -- voice_data.gate_gesture = pop()
 
-    -- lilts {
-    --     {"envar", "zz", 0.05, 0.2},
-    --     {"mul", "zz", "zz"}
-    -- }
+    lilts {
+        {"envar", "zz", 0.05, 0.2},
+        {"mul", "zz", "zz"}
+    }
 
 
     lilts {
