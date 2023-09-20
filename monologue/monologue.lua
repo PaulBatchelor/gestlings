91cef487bd8ea32c4cd420ad25e1769263f10407
paul <paul@noemail.net>
2023-09-20 20:00:07 +0000

    extracted monologue abstraction to separate module
    FossilOrigin-Name: cc05179deb4274b69c21446f82dd5c1cdc1f486b390940052e424b61515ebecc

---
+++ monologue/monologue.lua
@@ -0,0 +1,105 @@
+Monologue = {}
+
+local function phrase_to_mseq(morpheme, path, phrase, pros, vocab)
+    local mseq = {}
+    local merge = morpheme.merge
+
+    for _,ph in pairs(phrase) do
+
+        -- duration modifier
+        local dur = ph[2] or {1, 1}
+        local mrph = vocab[ph[1]]
+
+        -- merge partial morphemes
+        if ph[3] ~= nil then
+            for _, pm in pairs(ph[3]) do
+                mrph = merge(mrph, vocab[pm])
+            end
+        end
+
+        table.insert(mseq, {mrph, dur})
+    end
+
+    local mseq_dur = path.morphseq_dur(mseq)
+    -- print(mseq_dur[1], mseq_dur[2])
+
+    -- normalize: condense entire phrase into one beat
+    -- for some reason, we don't flip
+    -- best I can think of:
+    -- rescale each rate multiplier relative to the duration
+    -- divide each morpheme rate multiplier by the total duration
+    -- duration needs to be converted to rate (flip)
+    -- fraction division does an inversion on second operand (flip)
+    -- maybe those flips cancel out?
+    local scale = mseq_dur
+    for idx,_ in pairs(mseq) do
+        mseq[idx][2] = path.fracmul(mseq[idx][2], scale)
+
+        -- limited to 8-bit values
+        assert(mseq[idx][2][1] <= 0xFF)
+        assert(mseq[idx][2][2] <= 0xFF)
+    end
+
+    pros_scaled = {}
+    pros_scaled.pitch = path.scale_to_morphseq(pros.pitch, mseq)
+    pros_scaled.intensity = path.scale_to_morphseq(pros.intensity, mseq)
+
+    return mseq, pros_scaled
+end
+
+local function append_to_sequence(app, m, pros_pitch, pros_intensity, mseq, pros)
+    for _,mrph in pairs(mseq) do
+        local dur = mrph[2]
+        local mo = mrph[1]
+        app(m, dur, mo)
+    end
+
+    for _, v in pairs(pros.pitch) do
+        table.insert(pros_pitch, v)
+    end
+
+    for _, v in pairs(pros.intensity) do
+        table.insert(pros_intensity, v)
+    end
+end
+
+function Monologue.to_words(p)
+    local tal = p.tal
+    local path = p.path
+    local morpheme = p.morpheme
+    local vocab = p.vocab
+    local mono = p.monologue
+    local pros = p.prosody
+    local lookup = p.shapelut
+
+    local p_shapes = {}
+    local pros_pitch = {}
+    local pros_intensity = {}
+    local app = morpheme.appender(path)
+    local m = {} -- TODO rename
+
+    for _,stanza in pairs(mono) do
+        mseq, pros = phrase_to_mseq(morpheme, path, stanza[1], stanza[2], vocab, pm)
+        append_to_sequence(app, m, pros_pitch, pros_intensity, mseq, pros)
+    end
+
+    local words = {}
+    tal.begin(words)
+
+    tal.label(words, "hold")
+    tal.halt(words)
+    tal.jump(words, "hold")
+
+    morpheme.compile_noloop(tal, path, words, m, nil, lookup)
+
+    tal.label(words, "pros_pitch")
+    path.path(tal, words, pros_pitch)
+    tal.jump(tal, "hold")
+
+    tal.label(words, "pros_intensity")
+    path.path(tal, words, pros_intensity)
+    tal.jump(tal, "hold")
+    return words
+end
+
+return Monologue
--- scratch/junior_rt.lua
+++ scratch/junior_rt.lua
@@ -20,73 +20,7 @@ asset = asset:new{
     msgpack = require("util/MessagePack"),
     base64 = require("util/base64")
 }
-
-local function phrase_to_mseq(morpheme, path, phrase, pros, vocab)
-    local mseq = {}
-    local merge = morpheme.merge
-
-    for _,ph in pairs(phrase) do
-
-        -- duration modifier
-        local dur = ph[2] or {1, 1}
-        local mrph = vocab[ph[1]]
-
-        -- merge partial morphemes
-        if ph[3] ~= nil then
-            for _, pm in pairs(ph[3]) do
-                mrph = merge(mrph, vocab[pm])
-            end
-        end
-
-        table.insert(mseq, {mrph, dur})
-    end
-
-    local mseq_dur = path.morphseq_dur(mseq)
-    -- print(mseq_dur[1], mseq_dur[2])
-
-    -- normalize: condense entire phrase into one beat
-    -- for some reason, we don't flip
-    -- best I can think of:
-    -- rescale each rate multiplier relative to the duration
-    -- divide each morpheme rate multiplier by the total duration
-    -- duration needs to be converted to rate (flip)
-    -- fraction division does an inversion on second operand (flip)
-    -- maybe those flips cancel out?
-    local scale = mseq_dur
-    for idx,_ in pairs(mseq) do
-        mseq[idx][2] = path.fracmul(mseq[idx][2], scale)
-
-        -- limited to 8-bit values
-        assert(mseq[idx][2][1] <= 0xFF)
-        assert(mseq[idx][2][2] <= 0xFF)
-    end
-
-    pros_scaled = {}
-    pros_scaled.pitch = path.scale_to_morphseq(pros.pitch, mseq)
-    pros_scaled.intensity = path.scale_to_morphseq(pros.intensity, mseq)
-
-    return mseq, pros_scaled
-end
-
-local function append_to_sequence(app, m, pros_pitch, pros_intensity, mseq, pros)
-    for _,mrph in pairs(mseq) do
-        local dur = mrph[2]
-        local mo = mrph[1]
-        app(m, dur, mo)
-    end
-
-    for _, v in pairs(pros.pitch) do
-        table.insert(pros_pitch, v)
-    end
-
-    for _, v in pairs(pros.intensity) do
-        table.insert(pros_intensity, v)
-    end
-end
-
-function coord(x, y)
-    return (y - 1)*8 + x
-end
+local monologue = require("monologue/monologue")
 
 -- <@>
 function genvocab()
@@ -111,87 +45,18 @@ function genphrase(sentence)
     local sentence = sentence or { 1 }
     local phrase = {}
     local reg = {1, 1}
-    -- select the word
-    local wrd = coord(3, 2)
-    local pause = coord(1, 1)
 
     for _,word in pairs(sentence) do
         table.insert(phrase, {word, reg})
     end
-    -- for i=1,3 do
-    --     table.insert(phrase, {wrd, reg})
-    -- end
-    -- table.insert(phrase, {pause, reg})
+
     return phrase
 end
 -- </@>
 
-function monologue_to_words(p)
-    local tal = p.tal
-    local path = p.path
-    local morpheme = p.morpheme
-    local vocab = p.vocab
-    local mono = p.monologue
-    local pros = p.prosody
-    local lookup = p.shapelut
-
-    local p_shapes = {}
-    local pros_pitch = {}
-    local pros_intensity = {}
-    local app = morpheme.appender(path)
-    local m = {} -- TODO rename
-
-    for _,stanza in pairs(mono) do
-        mseq, pros = phrase_to_mseq(morpheme, path, stanza[1], stanza[2], vocab, pm)
-        append_to_sequence(app, m, pros_pitch, pros_intensity, mseq, pros)
-    end
-
-    local words = {}
-    tal.begin(words)
-
-    tal.label(words, "hold")
-    tal.halt(words)
-    tal.jump(words, "hold")
-
-    morpheme.compile_noloop(tal, path, words, m, nil, lookup)
-
-    tal.label(words, "pros_pitch")
-    path.path(tal, words, pros_pitch)
-    tal.jump(tal, "hold")
-
-    tal.label(words, "pros_intensity")
-    path.path(tal, words, pros_intensity)
-    tal.jump(tal, "hold")
-    return words
-end
-
 function genwords(data, phrase)
-    -- lil("shapemorfnew lut shapes/junior.b64")
-    -- lil("grab lut")
-    -- lut = pop()
-    -- lookup = shapemorf.generate_lookup(lut)
     local lookup = data.lookup
-
-    -- A='b275f8'
-    -- B='51f271'
-    -- C='9c6c5d'
-    -- D='5d71be'
-    -- E='ab8d71'
-
-    -- local vtx = path.vertex
-    -- local gm = gest.behavior.gliss_medium
-    -- local gl = gest.behavior.gliss
-    -- local lin = gest.behavior.linear
-    -- local stp = gest.behavior.step
-
-    -- local p_shapes = {}
-
-    local m = {}
-
-    local pm = data.pm
-
     local vocab = data.vocab
-
     pros = genpros()
 
     local question = pros.question
@@ -201,7 +66,7 @@ function genwords(data, phrase)
     local deflated = pros.deflated
     local excited = pros.excited
 
-    local monologue = {
+    local mono = {
         {phrase, neutral},
         {phrase, question},
         {phrase, some_jumps},
@@ -210,12 +75,12 @@ function genwords(data, phrase)
         {phrase, whisper},
     }
 
-    local words = monologue_to_words {
+    local words = monologue.to_words {
         tal = tal,
         path = path,
         morpheme = morpheme,
         vocab = vocab,
-        monologue = monologue,
+        monologue = mono,
         prosody = pros,
         shapelut = data.lookup
     }
@@ -422,31 +287,6 @@ function patch(words, data)
     G:done()
     cnd:unhold()
     glot:unhold()
-end
-
-consonance = {
-"b", "c", "d", "f", "g", "gh", "h", "k", "l", "m", "n",
-"p", "q", "r", "s", "t", "v", "w", "x", "y", "z",
-"zh", "th",
-}
-
-vowel = {
-"a", "e", "i", "o", "u", "y", "uo"
-}
-
-function generate_words(nwords)
-    local wordlist = {}
-    for _ = 1,nwords do
-        local v = vowel[math.random(#vowel)]
-        local c = consonance[math.random(#consonance)]
-        local word = c .. v
-        print (word)
-        -- wordlist[word] = true
-    end
-
-    -- for v,_ in pairs(wordlist) do
-    --     print(v)
-    -- end
 end
 
 function rtsetup()
