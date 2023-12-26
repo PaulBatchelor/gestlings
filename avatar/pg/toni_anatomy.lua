c17d61f4d01eee00912c8bbd7b08d14392e59095
paul <paul@noemail.net>
2023-12-26 20:14:32 +0000

    toni anatomy playground
    FossilOrigin-Name: f8dbf2eec89616a65b334c99e78f83a2514494773453859fd5be21cce38a275d

---
+++ avatar/pg/toni_anatomy.lua
@@ -0,0 +1,96 @@
+local mouth = require("avatar/mouth/mouth")
+local avatar = require("avatar/avatar")
+local sdfdraw = require("avatar/sdfdraw")
+local json = require("util/json")
+local core = require("util/core")
+local pprint = require("util/pprint")
+local anatomy = require("avatar/anatomy/anatomy")
+
+local lilt = core.lilt
+-- local sqrcirc = mouth:squirc()
+local asset = require("asset/asset")
+msgpack = require("util/MessagePack")
+asset = asset:new{
+    msgpack = msgpack,
+    base64 = require("util/base64")
+}
+
+lil("sdfvmnew vm")
+lil("grab vm")
+vm = pop()
+syms = sdfdraw.load_symbols(json)
+
+-- gestling_anatomy = asset:load("avatar/anatomy/a_junior.b64")
+
+local squirc = mouth:squirc()
+local scale = 0.6
+local squareoff = 0.5
+local shader = {
+    {
+        "point",
+        "vec2", 0.0, -0.3, "add2",
+        "scalar", 0.5, "circle"
+    },
+    {
+        "point",
+        "vec2", -0.5, 0.5 - 0.25,
+        "vec2", -0.5, -0.3 - 0.25,
+        "vec2", 0.5, -0.3 - 0.25,
+        "vec2", 0.5, 0.5 - 0.25,
+        "poly4"
+    },
+    "union",
+    "scalar 0 regset",
+    "scalar 0 regget",
+    {"scalar", 0.01, "onion"},
+
+    {
+        "point",
+        "vec2", 0.0, -0.3, "add2",
+        "vec2", 0.35, 0.25, "ellipse"
+    },
+    "scalar 0 regset",
+    "scalar 0 regget",
+    {"scalar", 0.01, "onion"},
+    "add",
+    {
+        "point",
+        "vec2", 0.0, -0.3, "add2",
+        "scalar", 0.1, "circle"
+    },
+    "add",
+    "gtz",
+
+    squirc:generate(scale, 2.0, 0.8, {0, 0.4}),
+
+    "gtz", "add",
+}
+
+
+an = anatomy.new {
+    syms = syms,
+    vm = vm,
+    sdfdraw = sdfdraw,
+    avatar = avatar,
+    lilt = lilt,
+    shader = shader,
+    asset = asset,
+    mouth_controller = mouth.name_to_mouth(squirc:name()),
+}
+
+
+av = anatomy.generate_avatar(an)
+
+lil("bpnew bp 240 320")
+avatar.setup(lilt)
+
+lilt {"bpset", "[grab bp]", 0, 0, 0, 240, 320}
+
+lilt {
+    "bpline", "[bpget [grab bp] 0]",
+    0, 320-60, 240, 320 - 60, 1
+}
+
+anatomy.apply_shape(an, "tri", 0.5)
+anatomy.draw(an)
+lil("bppng [grab bp] tmp/toni_anatomy.png")
